cdd158d0ec5eb690fad94134cbf8437a57e87102
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-19 12:51:02 +0200

    aplicacion_finalizadores
    Aplicación para aprender a utilizar finalizadores en C#

---
+++ DI/11_finalizadores/11_finalizadores/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _11_finalizadores {
+
+    class Primero {
+        ~Primero() {
+            System.Diagnostics.Trace.WriteLine("Llamada a primer Destructor");
+        }
+    }
+
+    class Segundo : Primero {
+        ~Segundo() {
+            System.Diagnostics.Trace.WriteLine("Llamada a Segundo Destructor");
+        }
+    }
+
+    class Tercero : Segundo {
+        ~Tercero() {
+            System.Diagnostics.Trace.WriteLine("Llamada a Tercer Destructor ");
+        }
+    }
+    class TestDestructors {
+        static void Main() {
+            Tercero t = new Tercero();
+        }
+    }
+}
