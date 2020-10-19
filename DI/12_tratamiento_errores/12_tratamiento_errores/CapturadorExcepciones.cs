a29eea77cf262bd687486d6bff544bc0b2c26b17
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-19 12:52:00 +0200

    aplicacion_tratamiento_errores
    Aplicación para aprender a gestionar el tratamiento de errores

---
+++ DI/12_tratamiento_errores/12_tratamiento_errores/CapturadorExcepciones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _12_tratamiento_errores {
+
+    class CapturadorExcepciones : Exception {
+
+        public CapturadorExcepciones() {}
+
+        public CapturadorExcepciones(string message) : base(message) {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ DI/12_tratamiento_errores/12_tratamiento_errores/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _12_tratamiento_errores {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("---------- Capturando errores ----------");
+
+            // Manejo de rrores 1
+            try {
+                int nn = 0;
+                nn = 4 / nn;
+            } catch (Exception e) { Console.WriteLine(e.Message); }
+
+            // Manejo de errores 2
+            try {
+
+                String diaSemana = "Lunes";
+
+                if (diaSemana.ToUpper().IndexOf("L") >= 0) {
+                    throw new CapturadorExcepciones("No me gustan los lunes");
+                }
+            } catch (Exception e) { Console.WriteLine(e.Message); }
+        }
+    }
+}
