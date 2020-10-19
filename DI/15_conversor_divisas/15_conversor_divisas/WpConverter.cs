667fb09101111d12f91f658c7e721e0f5be28277
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-19 13:02:20 +0200

    aplicacion_conversor_divisas
    Aplicación que convierte euros a dólares y viceversa utilizando la clase IValueConverter

---
+++ DI/15_conversor_divisas/15_conversor_divisas/WpConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.Diagnostics;
+
+namespace _15_conversor_divisas {
+    public class WpConverter : IValueConverter {
+
+        private const double diff = 1.17;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+
+            // Convierte de € a $ (Multiplica los € por 1.17)
+
+            String val = value.ToString();
+
+            if (val.Any()) {
+
+                val = val.Replace(".", ",");
+
+                double valdb = double.Parse(val);
+                double cambio = valdb * diff;
+                cambio = Math.Round(cambio, 2);
+                return $"{cambio.ToString()}";
+            } else { return ""; }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+
+            // Convierte de $ a € (Divide los $ entre 1.17)
+
+            String val = value.ToString();
+
+            if (val.Any()) {
+
+                val = val.Replace(".", ",");
+
+                double valdb = double.Parse(val);
+                double cambio = valdb / diff;
+                cambio = Math.Round(cambio, 2);
+                return $"{cambio.ToString()}";
+            } else { return ""; }
+        }
+    }
+}
