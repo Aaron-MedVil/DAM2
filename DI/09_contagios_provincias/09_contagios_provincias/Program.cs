75e5b2f7507ee71555901303e0edf4a21132755a
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-19 12:49:32 +0200

    aplicacion_contar_contagios
    Aplicación que utiliza diferentes componentes de C# para contabilizar el número de contagios en la comunidad de Castilla y León

---
+++ DI/09_contagios_provincias/09_contagios_provincias/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _09_contagios_provincias {
+
+    class Program {
+
+        // Array de contagios
+        public static Contagios[] contagios = new Contagios[9]; 
+
+        // Metodo main
+        static void Main(string[] args) {
+
+            Console.WriteLine("Inserte el nombre de la provincia de CyL o Fin para terminar:");
+            string nombreP = Console.ReadLine().ToUpper();
+
+            Registro rr = new Registro();
+
+            while (nombreP != "FIN") {
+
+                int indice = rr.obtenerIndice(nombreP); ;
+
+                contagios[indice].nombre = nombreP;
+
+                contagios[indice].contagios = (contagios[indice].contagios > 0) ? contagios[indice].contagios++ : 1;
+
+                Console.WriteLine($"Nº ingresos de la provincia {contagios[indice].nombre}: {contagios[indice].contagios}");
+
+                Console.WriteLine("Inserte el nombre de la provincia de CyL o Fin para terminar:");
+                nombreP = Console.ReadLine().ToUpper();
+            }
+
+            Console.WriteLine("Fin del programa");
+        }
+    }
+
+    // Estructura
+    public struct Contagios {
+
+        public int contagios; public String nombre;
+
+        // Constructor
+        public Contagios(int nContagios, String sProvincia) {
+            contagios = nContagios;
+            nombre = sProvincia;
+        }
+    }
+}
---
+++ DI/09_contagios_provincias/09_contagios_provincias/Registro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09_contagios_provincias {
+    class Registro {
+
+        // Enum de provincias
+        public enum provincias { BURGOS, SORIA, AVILA, VALLADOLID, SEGOVIA, LEON, SALAMANCA, ZAMORA };
+
+        // Busca el indice del enum de la provincia que se inserta
+        public int obtenerIndice(string nProvincia) {
+
+            int nn = (int)(Enum.Parse(typeof(provincias), nProvincia));
+            return nn;
+        }
+    }
+}
