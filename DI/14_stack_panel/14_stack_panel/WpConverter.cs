ddfaf3175d102852eff9fb0826d8ae734fa8843c
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-19 13:00:07 +0200

    aplicacion_stack_panel
    Aplicación para aprender el funcionamiento de los stack panel en WPF

---
+++ DI/14_stack_panel/14_stack_panel/WpConverter.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.Diagnostics;
+
+namespace _14_stack_panel {
+    public class WpConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+
+            Console.WriteLine("Converter " + value);
+            return Int32.Parse(value.ToString()) - 200;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+
+            throw new NotImplementedException();
+        }
+    }
+}
