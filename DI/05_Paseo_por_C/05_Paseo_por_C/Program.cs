38c0a52d43260f394bacab4066b52d1e1c86870d
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-19 12:43:12 +0200

    practica_paseo_C#
    Aplicación práctica de los apuntes de paseo por C#

---
+++ DI/05_Paseo_por_C/05_Paseo_por_C/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _05_Paseo_por_C {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Punto p = new Punto(123, 456);
+
+            p.visualizarPunto();
+            var s = Console.ReadKey();
+        }
+    }
+}
---
+++ DI/05_Paseo_por_C/05_Paseo_por_C/Punto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05_Paseo_por_C
+{
+    class Punto {
+
+        private int x, y;
+
+        public Punto(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void visualizarPunto() {
+            Console.WriteLine("Coordenadas punto: {0},{1}", x, y);
+        }
+    }
+}
