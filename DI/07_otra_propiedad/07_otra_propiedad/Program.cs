be5e87c8e763d2bd913663328115a4280d4b4412
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-19 12:45:31 +0200

    aplicacion_manejo_propiedades
    Aplicación de ejemplo para aprender el manejo de las propiedades de C#

---
+++ DI/07_otra_propiedad/07_otra_propiedad/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07_otra_propiedad {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("---------- Uso de propiedades ----------");
+
+            PropiedadEdad pe = new PropiedadEdad();
+
+            pe.tEdad = 55;
+            Console.WriteLine($"La edad es {pe.tEdad}");
+        }
+    }
+}
---
+++ DI/07_otra_propiedad/07_otra_propiedad/PropiedadEdad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07_otra_propiedad {
+
+    class PropiedadEdad {
+
+        private int edad;
+
+        public int tEdad {
+
+            get {
+
+                if (edad < 50)
+                    Console.WriteLine("Tu edad no es menor de 50");
+                    edad = 0;
+
+                return edad;
+            }
+            set { this.edad = (value < 50) ? 0 : value; }
+        }
+    }
+}
