a0c179e3ff08d4be48859134cd5e899f937fa6a5
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-19 12:36:00 +0200

    aplicacion_manejo_ventanas
    Aplicación de ejemplo para probar el manejo de ventanas WPF

---
+++ DI/03_Miercoles29/03_Miercoles29/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _03_Miercoles29
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mostrar_ventana(object sender, RoutedEventArgs e) {
+            
+            Window1 v = new Window1();
+            v.Show();
+        }
+    }
+}
