8cb095bed8b6efa86a8227b003d0332b9505ce2c
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-19 12:50:20 +0200

    aplicacion_herencias_clases
    Aplicación para ver el funcionamiento de las herencias en las clases de C#

---
+++ DI/10_herencia_clase/10_herencia_clase/MiBoton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Controls;
+
+namespace _10_herencia_clase {
+
+    class MiBoton : Button {
+
+        protected override void OnClick() {
+
+            this.Content = "Cambio de mensaje";
+            this.Width = 78;
+            this.Height = 54;
+            base.OnClick();
+        }
+    }
+}
