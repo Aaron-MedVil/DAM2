a7467abaf341e53d338a75532348fa4fe5faaee5
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-19 12:48:03 +0200

    aplicacion_comprobar_temperatura
    Aplicación para línea de comandos que devuelve un mensaje dependiendo de la tempera insertada

---
+++ DI/08_comprobar_temperatura/08_comprobar_temperatura/Pistola.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08_comprobar_temperatura {
+
+    class Pistola {
+
+        private int temperatura;
+
+        /**
+         * [Propiedad que calcula el resultado mediante la temperatura]
+        */
+        public string trabajo {
+
+            get {
+
+                if ( temperatura == -1 ) {
+
+                    // Cambia el color del texto de la consola a amarillo
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    return "Error de temperatura";
+                } else if (temperatura < 35) {
+
+                    // Cambia el color del texto de la consola a azul
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    return "Temperatura muy Fría";
+                } else if (temperatura > 37) {
+
+                    // Cambia el color del texto de la consola a rojo
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    return "Venga chaval, a tu casa";
+                } else {
+
+                    // Cambia el color del texto de la consola a verde
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    return "A tu clase";
+                }
+            } set { try { temperatura = int.Parse(value); } catch { temperatura = -1; } }
+        }
+    }
+}
---
+++ DI/08_comprobar_temperatura/08_comprobar_temperatura/Principal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _08_comprobar_temperatura {
+
+    class Principal {
+
+        static void Main(string[] args) {
+
+            Pistola p = new Pistola();
+            int bcl = 1;
+
+            // Bucle para medir mas temperaturas
+            while (bcl != 0) {
+
+                // Mide la temperatura
+                Console.Write("Inserte la temperatura medida:");
+                String tt = Console.ReadLine();
+                p.trabajo = tt;
+                Console.WriteLine($"{p.trabajo}");
+
+                // Pregunta al usuario si quiere seguir midiendo temperaturas
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write("¿Desea medir otra temperatura (0 para salir)?");
+                try { bcl = Convert.ToInt32(Console.ReadLine()); } catch { bcl = 1; }
+
+                // Devuelve el estilo predeterminado a la consola
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
