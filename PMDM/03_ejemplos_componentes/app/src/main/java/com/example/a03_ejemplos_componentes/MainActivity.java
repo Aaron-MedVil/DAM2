238363e2064262047b5971fc74ad5326c3be5afa
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-21 13:54:30 +0200

    Actualizacion del proyecto

--- PMDM/03_ejemplos_componentes/app/src/main/java/com/example/a03_ejemplos_componentes/MainActivity.java
+++ PMDM/03_ejemplos_componentes/app/src/main/java/com/example/a03_ejemplos_componentes/MainActivity.java
@@ -3,31 +3,46 @@ package com.example.a03_ejemplos_componentes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RadioButton rbSoltero, rbCasado, rbViudo, rbOtro;
     private TextView twResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        rbCasado = findViewById(R.id.rbCasado);
-        rbSoltero = findViewById(R.id.rbSoltero);
-        rbViudo = findViewById(R.id.rbViudo);
-        rbOtro = findViewById(R.id.rbOtro);
-        twResult = findViewById(R.id.twResult);
     }
 
-    public void estadoCivil() {
+    // Cambia el valor del TextView dependiendo del checkbox que marquemos
+    public void cambioEstadoCivil(View view) {
 
-        if (true) {
+        twResult = findViewById(R.id.twResult);
 
-            twResult.setText("hola");
+        Boolean checked = ((RadioButton) view).isChecked();
+        String sEstado = "";
+
+        switch (view.getId()) {
+            case R.id.rbCasado:
+                sEstado = (checked) ? "Casada/o" : "";
+                break;
+            case R.id.rbSoltero:
+                sEstado = (checked) ? "Soltera/o" : "";
+                break;
+            case R.id.rbViudo:
+                sEstado = (checked) ? "Viuda/o" : "";
+                break;
+            case R.id.rbOtro:
+                sEstado = (checked) ? "Otra/o" : "";
+                break;
+            default:
+                sEstado = "";
+                break;
         }
+
+        twResult.setText("Estado civil " + sEstado);
     }
 }
