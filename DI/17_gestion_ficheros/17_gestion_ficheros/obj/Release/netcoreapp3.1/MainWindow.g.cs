005138ec03a481ad58f7d30ed0340658d1c9ba06
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-22 11:52:41 +0200

    Cambios menores

--- DI/17_gestion_ficheros/17_gestion_ficheros/obj/Release/netcoreapp3.1/MainWindow.g.cs
+++ DI/17_gestion_ficheros/17_gestion_ficheros/obj/Release/netcoreapp3.1/MainWindow.g.cs
@@ -93,7 +93,7 @@ namespace _17_gestion_ficheros {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/17_gestion_ficheros;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/17_gestion_ficheros;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
