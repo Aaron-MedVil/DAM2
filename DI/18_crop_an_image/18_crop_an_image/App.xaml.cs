ccf226e3e0aa3a6091be51fb3318ef39d9120008
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-27 08:35:55 +0100

    pruebas de funcionamiento en nueva aplicacion

---
+++ DI/18_crop_an_image/18_crop_an_image/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _18_crop_an_image {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
---
+++ DI/18_crop_an_image/18_crop_an_image/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Drawing;
+
+namespace _18_crop_an_image {
+
+    public partial class MainWindow : Window {
+
+        public MainWindow() => InitializeComponent();
+
+        private void btnExaminar_Click(object sender, RoutedEventArgs e) {
+
+            MessageBox.Show("aqui");
+        }
+    }
+}
