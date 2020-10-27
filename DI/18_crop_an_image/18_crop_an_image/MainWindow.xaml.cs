ae2adaa9a916633f13e0189815636a4060c1bfe8
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-27 12:15:51 +0100

    Implementacion de nuevas funcionalidades

--- DI/18_crop_an_image/18_crop_an_image/MainWindow.xaml.cs
+++ DI/18_crop_an_image/18_crop_an_image/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
+using System.IO;
 using System.Windows;
-using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace _18_crop_an_image {
 
@@ -9,9 +11,77 @@ namespace _18_crop_an_image {
 
         public MainWindow() => InitializeComponent();
 
-        private void btnExaminar_Click(object sender, RoutedEventArgs e) {
+        // Carga una imagen definida
+        private void autoLoadImage(object sender, RoutedEventArgs e) {
 
-            MessageBox.Show("aqui");
+            limpiarContenedoresImagenes();
+
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(@Environment.CurrentDirectory + "/media/original.jpg", UriKind.RelativeOrAbsolute);
+            bi.EndInit();
+
+            Image img = new Image {
+                Stretch = Stretch.Fill,
+                Source = bi
+            };
+
+            gridImagen.Children.Add(img);
+            btnCropImage.Visibility = Visibility.Visible;
+        }
+
+        // Carga una imagen 
+        private void manualLoadImage(object sender, RoutedEventArgs e) {
+
+            limpiarContenedoresImagenes();
+
+            Microsoft.Win32.OpenFileDialog fileDialog = new Microsoft.Win32.OpenFileDialog() {
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Multiselect = false,
+                Filter = "All files (*.*)|*.*|Archivos JPG(*.jpg)|*.jpg|Archivos JPEG (*.jpeg) |*.jpeg|Archivos PNG (*.png)|*.png "
+            };
+
+            try {
+
+                fileDialog.ShowDialog();
+                Stream[] files = fileDialog.OpenFiles();
+
+                BitmapImage bi = new BitmapImage();
+                bi.BeginInit();
+                bi.UriSource = new Uri(fileDialog.FileName.ToString());
+                bi.EndInit();
+
+                Image img = new Image {
+                    Source = bi,
+                    Stretch = Stretch.Fill
+                };
+
+                gridImagen.Children.Add(img);
+                btnCropImage.Visibility = Visibility.Visible;
+            } catch { MessageBox.Show("Error"); }
+        }
+
+        // Carga las imagenes troceadas
+        private void cropImage(object sender, RoutedEventArgs e) {
+
+            MessageBox.Show("Cropped");
+        }
+
+        // Boton limpiar imagenes
+        private void cleanImages(object sender, RoutedEventArgs e) {
+
+            MessageBox.Show("Limpiar contenido");
+
+            btnCropImage.Visibility = Visibility.Collapsed;
+            limpiarContenedoresImagenes();
+        }
+
+        // Limpia los contenedores de imagenes
+        private void limpiarContenedoresImagenes() {
+
+            gridImagen.Children.RemoveRange(0, gridImagen.Children.Count - 1);
+            croppedImages.Children.RemoveRange(0, croppedImages.Children.Count - 1);
         }
     }
 }
