671144903111203a4ee7c0c2455bd16c5b0f5222
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-28 09:25:07 +0100

    cambios menores

--- PSP/procesos/src/Ejemplos/Ejemplo5.java
+++ PSP/procesos/src/Ejemplos/Ejemplo5.java
@@ -19,6 +19,7 @@ public class Ejemplo5 {
 
 		Process p = null;
 
+		// Comprobamos si enviamos por la linea de comandos algun argumento
 		if (args.length < 1) {
 
 			System.out.println("Se necesita un nombre de fichero");
@@ -26,36 +27,41 @@ public class Ejemplo5 {
 		} else {
 
 			try {
-
+				
+				// Abrimos el flujo para crear un fichero(El nombre del fichero es el especificado en los argumentos)
 				FileOutputStream fos = new FileOutputStream(args[0]);
+				
+				// Abre el flujo para escribir en el fichero creado caracter a caracter
 				PrintWriter pw = new PrintWriter(fos);
+				
+				// Ejecutamos el comando
 				p = r.exec(command);
 
+				// Leo la salida del comando
 				InputStream is = p.getInputStream();
+				
+				// Abrir el flujo para escribir un fichero linea a linea
 				BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
+				// Insertamos la salida del comando ejecutado linea a linea en el fichero que hayamos indicado
 				String linea;
 				while ((linea = br.readLine()) != null) {
-
-					System.out.println("Inserto en " + args[0] + " > " + linea);
 					pw.println(linea);
 				}
 
+				// Cerramos el flujo de datos
 				br.close();
 				pw.close();
-			} catch (Exception e) {
-				System.err.println("Error en el comando: " + command);
-				e.printStackTrace();
-			}
+			} catch (Exception e) { e.printStackTrace(); }
 
 			// COMPROBACION DEL ERROR (0-bien, 1-mal)
 			int exitVal;
 			try {
+				
+				// Espera a que el subproceso acabe de ejecutarse
 				exitVal = p.waitFor();
 				System.out.println("Valor de Salida: " + exitVal);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			} catch (InterruptedException e) { e.printStackTrace(); }
 		}
 	}
 }
