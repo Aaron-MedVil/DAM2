134be638a7614e38f3acc63422bf9556170faa2a
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-29 12:51:05 +0100

    Creacion de la aplicacion

---
+++ PMDM/06_alumno_curso_variacion/app/src/main/java/com/example/a06_alumno_curso_variacion/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.a06_alumno_curso_variacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView twResult;
+    Spinner spnAlumnos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        twResult = findViewById(R.id.twResult);
+        spnAlumnos = findViewById(R.id.spnAlumnos);
+
+        String[] arrCursos = getResources().getStringArray(R.array.arrCursos);
+
+        spnAlumnos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                twResult.setText(parent.getItemAtPosition(position).toString() + " - " + arrCursos[(int)id]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                twResult.setText("Resultado");
+            }
+        });
+    }
+}
