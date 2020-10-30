0ecab827d6aca270cf316d96b9bf1ca2338b90c2
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-30 12:58:17 +0100

    Creacion del proyecto

---
+++ DI/20_editor_texto/20_editor_texto/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace _20_editor_texto {
+    
+    public partial class MainWindow : Window {
+
+        public MainWindow() {
+
+            InitializeComponent();
+
+            for (int i = 0; i < 72; i = i + 4) {
+
+                // Esto es un ComboBox que se va a rellenar
+                // textSize.Items.add(i);
+            }
+        }
+    }
+}
