0b5089d8112266f9042a3ead81ea70dcb7ab7643
xaxe96 <a.medina.v.96@hotmail.com>
2020-10-30 10:06:55 +0100

    Creacion del proyecto

--- PMDM/06_alumno_curso_variacion/app/src/main/java/com/example/a06_alumno_curso_variacion/MainActivity.java
+++ PMDM/06_alumno_curso_variacion/app/src/main/java/com/example/a06_alumno_curso_variacion/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
