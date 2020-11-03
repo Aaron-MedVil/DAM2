5ba9c3d33d948ce68e87e9e7e88c1f3b5c5de5c8
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-03 12:19:06 +0100

    Cambios interfaz

--- DI/20_editor_texto/20_editor_texto/MainWindow.xaml.cs
+++ DI/20_editor_texto/20_editor_texto/MainWindow.xaml.cs
@@ -1,18 +1,28 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace _20_editor_texto {
-    
+
     public partial class MainWindow : Window {
 
         public MainWindow() {
 
             InitializeComponent();
+            /* // Ventana completa
+            WindowState = WindowState.Maximized;
+            WindowStyle = WindowStyle.None;
+            */
 
             for (int i = 0; i < 72; i = i + 4) {
 
                 // Esto es un ComboBox que se va a rellenar
                 // textSize.Items.add(i);
             }
+
+            this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
         }
+
+        // Funcion para cerrar la aplicacion
+        private void HandleEsc(object sender, KeyEventArgs e) { if (e.Key == Key.Escape) {Close();} }
     }
 }
