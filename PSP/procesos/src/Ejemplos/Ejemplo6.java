2aa19a9b78f2e6f9d0aedfe2dadc01fa72cd7a3d
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-04 08:41:45 +0100

    Creacion de nuevo proyecto

---
+++ PSP/procesos/src/Ejemplos/Ejemplo6.java
@@ -0,0 +1,27 @@
+package Ejemplos;
+
+import java.util.List;
+import java.util.Map;
+
+public class Ejemplo6 {
+
+	public static void main(String[] args) {
+		
+		ProcessBuilder pb = new ProcessBuilder();
+		
+		// El método enviroment del proceso nos devuelve las variables de entorno
+		Map entorno = pb.environment();
+		
+		System.out.println("Variables de entorno: ");
+		System.out.println(entorno);
+		
+		// Creamos el objeto ProcessBuilder con los parametros que queramos y guardamos el resultado en una lista 
+		pb = new ProcessBuilder("java", "Ejemplos/Ejemplo3", "\"Hola Mundo!!\"");
+		List<String> lista = pb.command();
+		
+		// Imprime por pantalla los elementos de la lista que se produce al ejecutar el comando
+		for (String l : lista) {
+			System.out.println(l);
+		}
+	}
+}
