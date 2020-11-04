a874fc13337698233021bbc0bd10f0f489198ac2
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-04 09:53:38 +0100

    Update Ejemplo8.java

--- PSP/procesos/src/Ejemplos/Ejemplo8.java
+++ PSP/procesos/src/Ejemplos/Ejemplo8.java
@@ -10,9 +10,11 @@ public class Ejemplo8 {
 		// Creamos la instancia de la clase ProcessBuilder y le pasamos por parametros el comando
 		ProcessBuilder pb = new ProcessBuilder("CMD", "/C", "DIR");
 		
+		// Especificamos los ficheros que queremos que se creen en caso de dar error y en caso de funcionar
 		File fOut = new File("D:\\Github\\DAM2\\PSP\\procesos\\src\\Resources\\Ejemplo8.txt");
 		File fErr = new File("D:\\Github\\DAM2\\PSP\\procesos\\src\\Resources\\Ejemplo8Err.txt");
 		
+		// Redirigimos las salidas a los ficheros y ejecutamos el comando
 		pb.redirectOutput(fOut);
 		pb.redirectError(fErr);
 		pb.start();
