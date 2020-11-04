aa7386a049c5890840e1af93a0828830ee9be616
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-04 09:43:19 +0100

    Create Ejemplo8.java

---
+++ PSP/procesos/src/Ejemplos/Ejemplo8.java
@@ -0,0 +1,15 @@
+package Ejemplos;
+
+import java.io.File;
+
+public class Ejemplo8 {
+
+	public static void main(String[] args) {
+		
+		// Creamos la instancia de la clase ProcessBuilder y le pasamos por parametros el comando
+		ProcessBuilder pb = new ProcessBuilder();
+		
+		File fOut = new File("D:\\Github\\DAM2\\PSP\\procesos\\src\\Resources\\Ejemplo8.txt");
+		File fErr = new File("D:\\Github\\DAM2\\PSP\\procesos\\src\\Resources\\Ejemplo8Err.txt");
+	}
+}
