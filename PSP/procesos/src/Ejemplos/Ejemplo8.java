9ae8c2553cbf2919c715cab2361c7ce845383dd1
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-04 09:45:54 +0100

    Update Ejemplo8.java

--- PSP/procesos/src/Ejemplos/Ejemplo8.java
+++ PSP/procesos/src/Ejemplos/Ejemplo8.java
@@ -7,9 +7,12 @@ public class Ejemplo8 {
 	public static void main(String[] args) {
 		
 		// Creamos la instancia de la clase ProcessBuilder y le pasamos por parametros el comando
-		ProcessBuilder pb = new ProcessBuilder();
+		ProcessBuilder pb = new ProcessBuilder("CMD", "/C", "DIR");
 		
 		File fOut = new File("D:\\Github\\DAM2\\PSP\\procesos\\src\\Resources\\Ejemplo8.txt");
 		File fErr = new File("D:\\Github\\DAM2\\PSP\\procesos\\src\\Resources\\Ejemplo8Err.txt");
+		
+		pb.redirectOutput(fOut);
+		pb.redirectError(fErr);
 	}
 }
