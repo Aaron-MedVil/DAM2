5864305ffd38666ecfd28783408fb5bfc6c72686
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-05 13:49:51 +0100

    Update

---
+++ PSP/procesos/src/Hilos/HilosPrimero.java
@@ -0,0 +1,12 @@
+package Hilos;
+
+public class HilosPrimero extends Thread {
+	
+	public void run() {
+		
+		for (int i = 0; i < 5; i++) {
+			
+			System.out.println("Dentro del hilo");
+		}
+	}
+}
---
+++ PSP/procesos/src/Hilos/UsaHilo.java
@@ -0,0 +1,18 @@
+// Para ejecutar primero compilamos la aplicacion desde la carpeta src con el comando "javac Hilos/UsaHilo.java"
+// Despues ejecutamos la aplicacion desde la carpeta src con el comando "java Hilos/UsaHilo"
+
+package Hilos;
+
+public class UsaHilo {
+
+	public static void main(String[] args) {
+		
+		HilosPrimero hp = new HilosPrimero();
+		hp.start();
+		
+		for (int i = 0; i < 5; i++) {
+			
+			System.out.println("Fuera del hilo");
+		}
+	}
+}
