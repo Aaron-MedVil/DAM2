de8a4c9d521fac60d154872073c3e014d6769b09
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-06 10:01:04 +0100

    Update

--- PMDM/Proyecto_Comercio/app/src/main/java/com/example/proyecto_comercio/MainActivity.java
+++ PMDM/Proyecto_Comercio/app/src/main/java/com/example/proyecto_comercio/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.proyecto_comercio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,11 +27,29 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         lvProductos = findViewById(R.id.lvProductos);
 
+        fillLayoutProductos();
+    }
+
+    // Funcion del evento de pulsar en un list view
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this, listProductos.get(position).getNombre(), Toast.LENGTH_SHORT).show();
+    }
+
+    // Rellena el layout de productos
+    public void fillLayoutProductos() {
+
+        // Obtenemos los valores del array de imagenes
+        TypedArray imgs = getResources().obtainTypedArray(R.array.productImages);
+
+        String nombre = "Haken - Virus";
+        String precio = "Min: 10€ - Max: 19€";
+
         // Insertamos los registros en el arraylist
         listProductos = new ArrayList<>();
-        listProductos.add(new Producto("Haken - Virus", "Min: 10€ - Max: 19€", R.drawable.haken_virus));
-        listProductos.add(new Producto("Haken - Virus", "Min: 10€ - Max: 19€", R.drawable.haken_virus));
-        listProductos.add(new Producto("Haken - Virus", "Min: 10€ - Max: 19€", R.drawable.haken_virus));
+        for (int i = 0; i < imgs.length(); i++) {
+            listProductos.add(new Producto(nombre, precio, imgs.getResourceId(i, 0)));
+        }
 
         // Asignamos los elementos del arraylist al layout
         MiAdaptadorProductos adapterProductos = new MiAdaptadorProductos(this, R.layout.productos_layout, listProductos);
@@ -37,11 +59,5 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         // Asignamos eventos al pulsar en cada list view
         lvProductos.setOnItemClickListener(this);
-    }
-
-    // Funcion del evento de pulsar en un list view
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(this, listProductos.get(position).getNombre(), Toast.LENGTH_SHORT).show();
     }
 }
