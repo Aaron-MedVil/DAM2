44384bb757d77253fd1756ab5add60b40c8c8b9d
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-11 09:41:25 +0100

    Update

--- DI/22_buscar_reemplazar_listerBox/22_buscar_reemplazar_listerBox/MainWindow.xaml.cs
+++ DI/22_buscar_reemplazar_listerBox/22_buscar_reemplazar_listerBox/MainWindow.xaml.cs
@@ -1,25 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _22_buscar_reemplazar_listerBox {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
+
+        Window1 vm = new Window1();
+
+        public MainWindow() => InitializeComponent();
+
+        private void Button_Click(object sender, RoutedEventArgs e) => vm.ShowDialog();
+
+        private void Buscar_reemplazar(object sender, RoutedEventArgs e) {
+
+            //buscar en el texto
+            vm.ShowDialog();
+
+            // MessageBox.Show(BuscarReemplazar.accion);
+
+            if (vm.accion == "Cancelar") { return; }
+
+            var textoBuscar = vm.palabraAbuscar.Text.Trim();
+            var textoCambio = vm.PalabraCambio.Text.Trim();
+
+            if (textoBuscar == "") { return; }
+
+            // Buscara palabras y remarcarlas las encontradas en rojo
+            if (textoCambio == "") {
+
+                CuadroTexto.SelectAll();
+                CuadroTexto.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(Colors.Black));
+                CuadroTexto.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
+
+                // https://docs.microsoft.com/es-es/dotnet/api/system.text.regularexpressions.regex?view=netframework-4.8
+
+                Regex reg = new Regex(textoBuscar.Trim(), RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                TextPointer position = CuadroTexto.Document.ContentStart;
+                List<TextRange> ranges = new List<TextRange>();
+
+                while (position != null) {
+
+                    if (position.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text) {
+
+                        string text = position.GetTextInRun(LogicalDirection.Forward);
+                        var matchs = reg.Matches(text);
+
+                        foreach (Match match in matchs) {
+
+                            TextPointer start = position.GetPositionAtOffset(match.Index);
+                            TextPointer end = start.GetPositionAtOffset("Hola".Trim().Length);
+                            TextRange textrange = new TextRange(start, end);
+                            ranges.Add(textrange);
+                        }
+                    }
+
+                    position = position.GetNextContextPosition(LogicalDirection.Forward);
+                }
+
+                foreach (TextRange range in ranges) {
+
+                    range.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(Colors.Red));
+                    range.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+                }
+            } else {
+
+                /* reemplazar */
+                // Sustituir Texto
+                var textRange = new TextRange(CuadroTexto.Document.ContentStart, CuadroTexto.Document.ContentEnd);
+                string rtf;
+
+                using (var memoryStream = new MemoryStream()) {
+
+                    textRange.Save(memoryStream, DataFormats.Rtf);
+                    rtf = ASCIIEncoding.Default.GetString(memoryStream.ToArray());
+                }
+
+                rtf = rtf.Replace(textoBuscar, textoCambio);
+                MemoryStream stream = new MemoryStream(ASCIIEncoding.Default.GetBytes(rtf));
+                CuadroTexto.SelectAll();
+                CuadroTexto.Selection.Load(stream, DataFormats.Rtf);
+            }
         }
     }
 }
---
+++ DI/22_buscar_reemplazar_listerBox/22_buscar_reemplazar_listerBox/Window1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace _22_buscar_reemplazar_listerBox {
+
+    public partial class Window1 : Window {
+
+        public string accion= String.Empty;
+
+        public Window1() => InitializeComponent();
+
+        // Acciones de los botones
+        private void Button_Click_Close(object sender, RoutedEventArgs e) {
+
+            Button bb = new Button();
+            bb = (Button)e.Source;
+
+            accion = (bb.Name == "Cancelar") ? "Cancelar" : "Ejecutar";
+
+            this.Close();
+        }
+    }
+}
