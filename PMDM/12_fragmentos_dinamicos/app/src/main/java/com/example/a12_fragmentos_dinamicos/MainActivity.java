c826bbd8be4d0c761bb07d4c5724314894c1828d
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-12 13:34:27 +0100

    Update

--- PMDM/12_fragmentos_dinamicos/app/src/main/java/com/example/a12_fragmentos_dinamicos/MainActivity.java
+++ PMDM/12_fragmentos_dinamicos/app/src/main/java/com/example/a12_fragmentos_dinamicos/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.a12_fragmentos_dinamicos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentTransaction transaccion;
+    Fragment initialFragment, redFragment, greenFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialFragment = new FragmentoInicial();
+        redFragment = new FragmentoRojo();
+        greenFragment = new FragmentoVerde();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.containerFragments, initialFragment).commit();
+    }
+
+    public void cambiarFragmento(View view) {
+
+        transaccion = getSupportFragmentManager().beginTransaction();
+
+        switch (view.getId()) {
+            case R.id.btnRojo:
+                transaccion.replace(R.id.containerFragments, redFragment).commit();
+                transaccion.addToBackStack(null);
+                break;
+            case R.id.btnVerde:
+                transaccion.replace(R.id.containerFragments, greenFragment).commit();
+                transaccion.addToBackStack(null);
+                break;
+        }
     }
 }
