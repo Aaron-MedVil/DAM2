c0e323be0f5bff7f18bce4f5c6b9edf37cfb46e9
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-16 20:46:34 +0100

    Update

---
+++ DI/24_repaso_ut1/24_repaso_ut1/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace _24_repaso_ut1 {
+
+    public partial class MainWindow : Window {
+
+        public MainWindow() => InitializeComponent();
+
+        private void btn1_Click(object sender, RoutedEventArgs e) {
+
+            ManejoJSON w1 = new ManejoJSON();
+            w1.Show();
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
---
+++ DI/24_repaso_ut1/24_repaso_ut1/ManejoJSON.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using Newtonsoft.Json;
+using System;
+using System.Reflection.Metadata;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+namespace _24_repaso_ut1 {
+
+    public partial class ManejoJSON : Window {
+
+        List<Pokemones> ListaPokemones = new List<Pokemones>();
+
+        public ManejoJSON() => InitializeComponent();
+
+        // Abrir un fichero JSON y mostrarlo en un DataGrid
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+
+            string filemone = Environment.CurrentDirectory + "/res/pokemones.json";
+            string json = File.ReadAllText(filemone);
+
+            ListaPokemones = JsonConvert.DeserializeObject<List<Pokemones>>(json);
+            dgJson.ItemsSource = ListaPokemones;
+        }
+
+        private void mostrarDatosPokemon(object sender, MouseButtonEventArgs e) {}
+    }
+}
