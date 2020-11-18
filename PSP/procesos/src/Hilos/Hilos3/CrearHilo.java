9d2a9f2b0fe75d49b8378f0b50b009c4f8bffd79
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-18 09:34:49 +0100

    Update CrearHilo.java

--- PSP/procesos/src/Hilos/Hilos3/CrearHilo.java
+++ PSP/procesos/src/Hilos/Hilos3/CrearHilo.java
@@ -2,5 +2,10 @@ package Hilos.Hilos3;
 
 public class CrearHilo extends Thread {
 	
-	
+	public void run() {
+		
+		System.out.println("Ejecución del hilo " + this.getName());
+		System.out.println("Prioridad del hilo " + this.getPriority());
+		System.out.println("Identificador del hilo " + this.getId());
+	}
 }
