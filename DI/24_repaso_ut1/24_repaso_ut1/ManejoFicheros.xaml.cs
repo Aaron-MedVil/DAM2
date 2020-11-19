f77a168053d65748da223c8bf9b30b0b705947cd
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-19 12:54:42 +0100

    Update

--- DI/24_repaso_ut1/24_repaso_ut1/ManejoFicheros.xaml.cs
+++ DI/24_repaso_ut1/24_repaso_ut1/ManejoFicheros.xaml.cs
@@ -6,5 +6,13 @@ namespace _24_repaso_ut1 {
 
         /// <summary>Funcion que carga los componentes de la pagina</summary>
         public ManejoFicheros() => InitializeComponent();
+
+        /// <summary>Funcion quese ejecuta cuando se carga la ventana</summary>
+        /// <param name="sender">Objeto que ejecuta la accion</param>
+        /// <param name="e">Objeto evento</param>
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+
+            // Open file dialog para seleccionar un fichero
+        }
     }
 }
