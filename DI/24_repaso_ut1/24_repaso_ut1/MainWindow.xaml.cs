6de79b9bf113aebcff927b1f5c87ab7b3c2b5093
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-19 09:08:01 +0100

    Update

--- DI/24_repaso_ut1/24_repaso_ut1/MainWindow.xaml.cs
+++ DI/24_repaso_ut1/24_repaso_ut1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace _24_repaso_ut1 {
 
@@ -10,7 +11,7 @@ namespace _24_repaso_ut1 {
         /// <summary>Abre la ventana de manejo de JSON</summary>
         /// <param name="sender">Objeto que ejecuta la accion</param>
         /// <param name="e">Objeto evento</param>
-        private void btn1_Click(object sender, RoutedEventArgs e) {
+        private void btnManejoJson_Click(object sender, RoutedEventArgs e) {
 
             ManejoJSON w1 = new ManejoJSON();
             w1.Show();
@@ -19,10 +20,26 @@ namespace _24_repaso_ut1 {
         /// <summary>Abre la ventana de manejo de canvas</summary>
         /// <param name="sender">Objeto que ejecuta la accion</param>
         /// <param name="e">Objeto evento</param>
-        private void btn2_Click(object sender, RoutedEventArgs e) {
+        private void btnManejoCanvas_Click(object sender, RoutedEventArgs e) {
 
             VentanaCanvas vc = new VentanaCanvas();
             vc.Show();
+        }
+
+        /// <summary>Abre la pagina de manejo de ficheros</summary>
+        /// <param name="sender">Objeto que ejecuta la accion</param>
+        /// <param name="e">Objeto evento</param>
+        private void btnManejoFicheros_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        /// <summary>Cambia el estilo de los elementos de la pagina principal</summary>
+        /// <param name="sender">Objeto que ejecuta la accion</param>
+        /// <param name="e">Objeto evento</param>
+        private void btnCambiarEstilos_Click(object sender, RoutedEventArgs e) {
+
+            Button btn = (Button)sender;
+            btn.Style = (Style)(this.Resources["cambioBtnStyle"]);
         }
     }
 }
