4e21450c81eda40ae1049bc6b6c3c087b1a31196
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-19 13:03:43 +0100

    Update

--- PMDM/14_Dialogs/app/src/main/java/com/example/a14_dialogs/MainActivity.java
+++ PMDM/14_Dialogs/app/src/main/java/com/example/a14_dialogs/MainActivity.java
@@ -3,19 +3,22 @@ package com.example.a14_dialogs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private CharSequence[] csGo = {"Chrome", "Edge", "Internete Explorer", "Opera", "Firefox"};
-    private ArrayList idiomas = new ArrayList();
+    private ArrayList idiomasSelecteds = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,14 +92,14 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             case R.id.btnCheckedDialog:
-                idiomas.clear();
+                idiomasSelecteds.clear();
                 AlertDialog.Builder adBtnChecked = new AlertDialog.Builder(this);
-                adBtnChecked.setTitle("Conocimiento en idiomas");
+                adBtnChecked.setTitle("Conocimiento en idiomasSelecteds");
                 adBtnChecked.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         String mensaje = "Idiomas que conoces:";
-                        for (Object idioma:idiomas) { mensaje += "\n- " + idioma; }
+                        for (Object idioma:idiomasSelecteds) { mensaje += "\n- " + idioma; }
                         Toast.makeText(getApplication(), mensaje, Toast.LENGTH_SHORT).show();
                     }
                 });
@@ -104,9 +107,9 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which, boolean isChecked) {
                         String option = getResources().getStringArray(R.array.idiomas)[which];
-                        if (isChecked) { idiomas.add(option); }
-                        else if(idiomas.contains(option)) { idiomas.remove(option); }
-                        Toast.makeText(getApplication(), "Opciones seleccionadas " + idiomas.size(), Toast.LENGTH_SHORT).show();
+                        if (isChecked) { idiomasSelecteds.add(option); }
+                        else if(idiomasSelecteds.contains(option)) { idiomasSelecteds.remove(option); }
+                        Toast.makeText(getApplication(), "Opciones seleccionadas " + idiomasSelecteds.size(), Toast.LENGTH_SHORT).show();
                     }
                 });
                 adBtnChecked.show();
@@ -116,6 +119,12 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             case R.id.btnDatePickerDialog:
+                Calendar calendar = Calendar.getInstance();
+                DatePickerDialog dgDatePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    }
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
                 break;
 
             case R.id.btnTimePickerDialog:
