d1eb2f435bd5aa3728c0e5983befaa879d119f38
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-23 12:13:56 +0100

    Update

--- DI/20_editor_texto/20_editor_texto/GestDoc.cs
+++ DI/20_editor_texto/20_editor_texto/GestDoc.cs
@@ -1,11 +1,171 @@
-﻿namespace _20_editor_texto {
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace _20_editor_texto {
 
     class GestDoc {
 
         private MainWindow mw;
+        private BrushConverter bc = new BrushConverter();
+        private string nomDocumento;
 
         /// <summary>Constructor de la clase</summary>
         /// <param name="mw">Instancia de la clase MainWindow</param>
         public GestDoc(MainWindow mw) => this.mw = mw;
+
+        /// <summary>Vacia el cajon de texto</summary>
+        public void vaciarCajaTexto() => mw.cajaTexto.Document.Blocks.Clear();
+
+        /// <summary>Guarda un fichero nuevo</summary>
+        /// <param name="nomdoc">Ruta del documento que vamos a crear</param>
+        /// <returns type="Boolean">True si el fichero se ha creado correctamente</returns>
+        public bool generarnuevoFichero(string? nomdoc) {
+
+            if (nomdoc == "" || nomdoc == null) {
+
+                SaveFileDialog dlg = new SaveFileDialog { Filter = "rtf (*.rtf)|*.rtf|txt (*.txt)|*.txt" };
+
+                if (dlg.ShowDialog() == true) {
+
+                    mw.nomDocumento = dlg.FileName;
+
+                    TextRange t = new TextRange(mw.cajaTexto.Document.ContentStart, mw.cajaTexto.Document.ContentEnd);
+                    FileStream file = new FileStream(mw.nomDocumento, FileMode.Create);
+                    t.Save(file, DataFormats.XamlPackage);
+                    file.Close();
+
+                    return true;
+                } else { return false; }
+            } else {
+
+                TextRange t = new TextRange(mw.cajaTexto.Document.ContentStart, mw.cajaTexto.Document.ContentEnd);
+                FileStream file = new FileStream(nomdoc, FileMode.Create);
+                t.Save(file, DataFormats.XamlPackage);
+                file.Close();
+                return true;
+            }
+        }
+
+        /// <summary>Imprime un documento</summary>
+        /// <returns type="Boolean">True si el fichero se imprime correctamente</returns>
+        public bool printDoc() {
+
+            PrintDialog pd = new PrintDialog();
+
+            if (pd.ShowDialog() == true) {
+
+                pd.PrintVisual(mw.cajaTexto as Visual, "VistaPrevia");
+                pd.PrintDocument(((IDocumentPaginatorSource)mw.cajaTexto.Document).DocumentPaginator, "Impresión del Documento");
+
+                return true;
+            } else { return false; }
+        }
+
+        /// <summary>Cambia el tema de la aplicacion</summary>
+        /// <param name="theme">Tema implementado actualmente</param>
+        /// <returns type="String">Nombre del tema que hemos implementado</returns>
+        public string setTheme(string theme) {
+
+            switch (theme) {
+                case "light":
+                    mw.miChangeEditorBackground.Header = "Modo noche";
+                    // mw.Background = (Brush)bc.ConvertFrom(c.light());
+                    return "dark";
+                default:
+                    mw.miChangeEditorBackground.Header = "Modo día";
+                    // mw.Background = (Brush)bc.ConvertFrom(c.dark());
+                    return "light";
+            }
+        }
+
+        /// <summary>Abre un fichero</summary>
+        /// <param name="nomdoc">Ruta del documento que vamos a abrir</param>
+        /// <returns Type="Boolean">True si el fichero se abre correctamente</returns>
+        public bool openDoc(string? nomdoc) {
+
+            if (nomdoc == "" || nomdoc == null) {
+
+                OpenFileDialog dlg = new OpenFileDialog {
+                    DefaultExt = ".rtf",
+                    Filter = "Fichero de texto|*.txt|Fichero de texto enriquecido|*.rtf"
+                };
+
+                if (dlg.ShowDialog() == true) {
+
+                    vaciarCajaTexto();
+
+                    mw.nomDocumento = dlg.FileName;
+                    TextRange t = new TextRange(mw.cajaTexto.Document.ContentStart, mw.cajaTexto.Document.ContentEnd);
+
+                    FileStream file = new FileStream(mw.nomDocumento, FileMode.Open);
+                    t.Load(file, DataFormats.XamlPackage);
+                    file.Close();
+
+                    return true;
+                } else { return false; }
+            } else {
+
+                vaciarCajaTexto();
+
+                File.WriteAllText(nomdoc, new TextRange(mw.cajaTexto.Document.ContentStart, mw.cajaTexto.Document.ContentEnd).Text);
+                return true;
+            }
+        }
+
+        /// <summary>Cambia las propiedades del estilo de un texto</summary>
+        /// <param name="prop">Propiedad que vamos a cambiar</param>
+        public void changePropText(string? prop) {
+
+            if (prop != "" || prop != null) {
+
+                TextRange rango = new TextRange(mw.cajaTexto.Selection.Start, mw.cajaTexto.Selection.End);
+
+                if (prop == "bold") {
+
+                    var propertyValue = rango.GetPropertyValue(TextElement.FontWeightProperty);
+
+                    if (propertyValue.ToString() == "Normal") {
+                        rango.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+                    } else {
+                        rango.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
+                    }
+                }
+                else if (prop == "italic") {
+
+                    var propertyValue = rango.GetPropertyValue(TextElement.FontStyleProperty);
+
+                    if (propertyValue.ToString() == "Normal") {
+                        rango.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Italic);
+                    } else {
+                        rango.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Normal);
+                    }
+                }
+                else if (prop == "underline") {
+
+                    var propertyValue = rango.GetPropertyValue(TextBlock.TextDecorationsProperty);
+
+                    if (propertyValue.ToString() != "Baseline") {
+                        rango.ApplyPropertyValue(TextBlock.TextDecorationsProperty, TextDecorations.Baseline);
+                    } else {
+                        rango.ApplyPropertyValue(TextBlock.TextDecorationsProperty, null);
+                    }
+                }
+                else if (prop == "color") {
+
+                    // Color coloruso = (Color)selectorColor.SelectedColor;
+                    // Brush mibrush = new SolidColorBrush(coloruso);
+                    // rango.ApplyPropertyValue(TextElement.ForegroundProperty, mibrush);
+                }
+                else if (prop == "fontSize") {
+
+                    // rango.ApplyPropertyValue(TextElement.FontSizeProperty, (int)mw.cbFontSize.SelectedItem);
+                }
+                else if (prop == "fontFamily") {}
+            } else { MessageBox.Show("Propiedad no definida"); }
+        }
     }
 }
--- DI/20_editor_texto/20_editor_texto/MainWindow.xaml.cs
+++ DI/20_editor_texto/20_editor_texto/MainWindow.xaml.cs
@@ -1,11 +1,15 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace _20_editor_texto {
 
     public partial class MainWindow : Window {
 
         private GestDoc gestDoc;
-
         private string theme = "light";
         public string? nomDocumento;
 
@@ -15,6 +19,130 @@ namespace _20_editor_texto {
         /// <summary>Funcion que se ejecuta cuando se carga la ventana</summary>
         /// <param name="sender">Objeto que produce el evento</param>
         /// <param name="e">Objeto con la informacion del evento</param>
-        private void mainWindowOnLoad(object sender, RoutedEventArgs e) {}
+        private void mainWindowOnLoad(object sender, RoutedEventArgs e) {
+
+            gestDoc = new GestDoc(this);
+            fillMenuComboBox();
+            theme = gestDoc.setTheme(theme);
+            cajaTexto.SpellCheck.IsEnabled = true;
+        }
+
+        /// <summary>Rellena la informacion de lo ComboBox del menu</summary>
+        private void fillMenuComboBox() {
+
+            // Rellena el ComboBox del tamaño de letra
+            for (int i = 4; i < 72; i = i + 4) { cbFontSize.Items.Add(i); }
+            cbFontSize.Text = "20";
+
+            // Rellena el ComboBox de la fuente
+            foreach (FontFamily fuente in Fonts.SystemFontFamilies) { cbFontType.Items.Add(fuente); }
+            cbFontType.Text = "Comic Sans MS";
+
+            // Asigna el default al editor
+            cajaTexto.FontSize = (int)cbFontSize.SelectedItem;
+            cajaTexto.FontFamily = new FontFamily(cbFontType.SelectedItem.ToString());
+        }
+
+        /// <summary>Funcion para poner la ventana en FullScreen</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void pantallaCompleta(object sender, RoutedEventArgs e) {
+
+            // Cambia el texto del MenuItem de cambiar a pantalla completa
+            miFullScreen.Header = (WindowStyle.ToString() != "None") ? "Salir de pantalla completa" : "Pantalla completa";
+
+            // Si la ventana esta en pantalla completa lo pone en ventana con marcos y viceversa
+            WindowStyle = (WindowStyle.ToString() != "None") ? WindowStyle.None : WindowStyle.SingleBorderWindow;
+            Hide(); Show();
+        }
+
+        /// <summary>Ejecuta un documento que seleccionemos</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void ejecutarAyuda(object sender, RoutedEventArgs e) {
+
+            string ruta = @"D:\Github\DAM2\DI\20_editor_texto\20_editor_texto\bin\Debug\netcoreapp3.1\resources\ago_doc.pdf";
+
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo($"{ruta}") { UseShellExecute = true };
+            p.Start();
+        }
+
+        /// <summary>Llama al metodo para cambiar el tema</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void changeTheme(object sender, RoutedEventArgs e) => theme = gestDoc.setTheme(theme);
+
+        /// <summary>Evento que llama a la funcion para crear un evento nuevo</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void nuevoDocumento(object sender, ExecutedRoutedEventArgs e) {
+
+            if (gestDoc.generarnuevoFichero(nomDocumento)) {
+
+                MessageBox.Show("Fichero guardado correctamente");
+                gestDoc.vaciarCajaTexto();
+                nomDocumento = null;
+            } else { MessageBox.Show("Error al guardar el fichero"); }
+        }
+
+        /// <summary>Guarda un documento</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void guardarDocumento(object sender, ExecutedRoutedEventArgs e) {
+
+            if (gestDoc.generarnuevoFichero(nomDocumento)) { MessageBox.Show("Fichero guardado correctamente"); }
+            else { MessageBox.Show("Error al guardar el fichero"); }
+        }
+
+        /// <summary>Funcion para cerrar la aplicacion con el boton escape</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void salirPrograma(object sender, ExecutedRoutedEventArgs e) => Close();
+
+        /// <summary>Evento que controla que se imprima correctamente el documento</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void imprimirDocumento(object sender, ExecutedRoutedEventArgs e) {
+
+            if (gestDoc.printDoc() == true) { MessageBox.Show("Documento impreso correctamente"); }
+            else { MessageBox.Show("Error al imprimir el documento"); }
+        }
+
+        /// <summary>Evento que controla que se abra correctamente un documento</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void abrirDocumento(object sender, ExecutedRoutedEventArgs e) {
+
+            if (gestDoc.openDoc(nomDocumento) == true) { MessageBox.Show("Fichero abierto correctamente"); }
+            else { MessageBox.Show("Error al abrir el documento"); }
+        }
+
+        /// <summary>Activa la corrección ortográfica en tiempo real</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void toggleSpellCheck(object sender, RoutedEventArgs e) => cajaTexto.SpellCheck.IsEnabled = !cajaTexto.SpellCheck.IsEnabled;
+
+        /// <summary>Activa/Desactiva la propiedad negrita del texto seleccionado</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void toggleBoldText(object sender, ExecutedRoutedEventArgs e) => gestDoc.changePropText("bold");
+
+        /// <summary>Activa/Desactiva la propiedad cursiva del texto seleccionado</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void toggleItalicText(object sender, ExecutedRoutedEventArgs e) => gestDoc.changePropText("italic");
+
+        /// <summary>Activa/Desactiva la propiedad subrayado del texto seleccionado</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void toggleUnderlineText(object sender, ExecutedRoutedEventArgs e) => gestDoc.changePropText("underline");
+
+        /// <summary>Cambia el color del texto seleccionado</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void EventoColor(object sender, RoutedPropertyChangedEventArgs<Color?> e) => gestDoc.changePropText("color");
+
+        private void fontSizeChanged(object sender, SelectionChangedEventArgs e) => gestDoc.changePropText("fontSize");
     }
 }
