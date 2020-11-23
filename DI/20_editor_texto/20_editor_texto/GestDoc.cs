0586a4fa7c9f5581217f5037b7b8aab917709984
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-23 09:14:10 +0100

    Create proyect

---
+++ DI/20_editor_texto/20_editor_texto/GestDoc.cs
@@ -0,0 +1,11 @@
+﻿namespace _20_editor_texto {
+
+    class GestDoc {
+
+        private MainWindow mw;
+
+        /// <summary>Constructor de la clase</summary>
+        /// <param name="mw">Instancia de la clase MainWindow</param>
+        public GestDoc(MainWindow mw) => this.mw = mw;
+    }
+}
---
+++ DI/20_editor_texto/20_editor_texto/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace _20_editor_texto {
+
+    public partial class MainWindow : Window {
+
+        private GestDoc gestDoc;
+
+        private string theme = "light";
+        public string? nomDocumento;
+
+        /// <summary>Funcion para cargar los componentes de la ventana</summary>
+        public MainWindow() => InitializeComponent();
+
+        /// <summary>Funcion que se ejecuta cuando se carga la ventana</summary>
+        /// <param name="sender">Objeto que produce el evento</param>
+        /// <param name="e">Objeto con la informacion del evento</param>
+        private void mainWindowOnLoad(object sender, RoutedEventArgs e) {}
+    }
+}
