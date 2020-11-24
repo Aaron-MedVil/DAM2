2cea4f9dca4adf5efdb509edafd54aa559b8a573
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-24 12:41:30 +0100

    Update CrearHiloRunnable.java

--- PSP/procesos/src/Hilos/Hilos4/CrearHiloRunnable.java
+++ PSP/procesos/src/Hilos/Hilos4/CrearHiloRunnable.java
@@ -1,9 +1,12 @@
 package Hilos.Hilos4;
 
 public class CrearHiloRunnable implements Runnable {
-	
+
+	private int id;
+
+	public CrearHiloRunnable(int id) { this.id = id; }
+
 	public void run() {
-		
-		for (int i = 0; i < 3; i++) { System.out.println("Hilo_Runnable[" + i + "]"); }
+		for (int i = 0; i < 100; i++) { System.out.println("[R]Ejecutándose hilo: " + id + " Contador: " + i); }
 	}
 }
