fcd8d465757646bc7fbc678a07cbf5606f7b313f
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-24 13:41:22 +0100

    Update Ejemplo_03.java

--- AD/UT1/src/Ejemplos/Ejemplo_03.java
+++ AD/UT1/src/Ejemplos/Ejemplo_03.java
@@ -5,9 +5,14 @@ public class Ejemplo_03 {
 	public static void main(String[] args) {
 		
 		Ejemplo_03_Vehiculo minivan = new Ejemplo_03_Vehiculo(9, 50, 18);
+		Ejemplo_03_Vehiculo sportCar = new Ejemplo_03_Vehiculo(10, 25, 30);
 		
 		System.out.println("La minivan puede llevar " + minivan.getPasajeros() + " "
 				+ "pasajeros con un rango de " + minivan.rango() + " "
+				+ "Kilómetros con el depósito lleno");
+		
+		System.out.println("El coche deportivo puede llevar " + sportCar.getPasajeros() + " "
+				+ "pasajeros con un rango de " + sportCar.rango() + " "
 				+ "Kilómetros con el depósito lleno");
 	}
 }
