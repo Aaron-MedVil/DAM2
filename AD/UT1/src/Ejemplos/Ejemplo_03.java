a065f1924ef29aa57a19699ff13658668042c606
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-24 13:52:20 +0100

    Update Ejemplo_03.java

--- AD/UT1/src/Ejemplos/Ejemplo_03.java
+++ AD/UT1/src/Ejemplos/Ejemplo_03.java
@@ -1,18 +1,21 @@
 package Ejemplos;
 
+import java.util.ArrayList;
+
 public class Ejemplo_03 {
 
 	public static void main(String[] args) {
 		
-		Ejemplo_03_Vehiculo minivan = new Ejemplo_03_Vehiculo(9, 50, 18);
-		Ejemplo_03_Vehiculo sportCar = new Ejemplo_03_Vehiculo(10, 25, 30);
+		ArrayList<Ejemplo_03_Vehiculo> vehiculos = new ArrayList<Ejemplo_03_Vehiculo>();
+		vehiculos.add(new Ejemplo_03_Vehiculo("Minivan", 9, 50, 18));
+		vehiculos.add(new Ejemplo_03_Vehiculo("Coche deportivo", 10, 25, 30));
 		
-		System.out.println("La minivan puede llevar " + minivan.getPasajeros() + " "
-				+ "pasajeros con un rango de " + minivan.rango() + " "
-				+ "Kilómetros con el depósito lleno");
-		
-		System.out.println("El coche deportivo puede llevar " + sportCar.getPasajeros() + " "
-				+ "pasajeros con un rango de " + sportCar.rango() + " "
-				+ "Kilómetros con el depósito lleno");
+		for (Ejemplo_03_Vehiculo vehiculo : vehiculos) {
+			
+			System.out.println("El vehículo" + vehiculo.getNombre() + " "
+					+ "puede llevar " + vehiculo.getPasajeros() + " "
+					+ "pasajeros con un rango de " + vehiculo.rango() + " "
+					+ "Kilómetros con el depósito lleno");
+		}
 	}
 }
