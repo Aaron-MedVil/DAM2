6b86ffce2cc3f1df75184b97e51bca47e9f0dd28
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-24 13:01:13 +0100

    Update Ejemplo_01.java

--- AD/UT1/src/Ejemplos/Ejemplo_01.java
+++ AD/UT1/src/Ejemplos/Ejemplo_01.java
@@ -1,9 +1,28 @@
 package Ejemplos;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Ejemplo_01 {
 
 	public static void main(String[] args) {
-		
-		
+
+		File fichero1 = new File("src\\Ejemplos\\Files\\file.txt");
+
+		try {
+
+			// Comprobamos si el fichero se crea
+			if (fichero1.createNewFile()) {
+
+				System.out.println("Fichero creado");
+			} else {
+
+				System.err.println("Fichero no creado");
+			}
+		} catch (IOException e) {
+
+			// Imprime la excepcion si se produce algun error
+			e.printStackTrace();
+		}
 	}
 }
