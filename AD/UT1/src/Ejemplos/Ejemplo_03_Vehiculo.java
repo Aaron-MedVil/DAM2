15d9575f24c42a13c1038a1fac8e4acf22a8fe60
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-24 13:36:25 +0100

    Update Ejemplo_03_Vehiculo.java

--- AD/UT1/src/Ejemplos/Ejemplo_03_Vehiculo.java
+++ AD/UT1/src/Ejemplos/Ejemplo_03_Vehiculo.java
@@ -2,7 +2,7 @@ package Ejemplos;
 
 public class Ejemplo_03_Vehiculo {
 
-	int pasajeros, capacidad, lpkm;
+	private int pasajeros, capacidad, lpkm;
 	
 	public Ejemplo_03_Vehiculo(int pasajeros, int capacidad, int lpkm) {
 		this.pasajeros = pasajeros;
@@ -10,31 +10,14 @@ public class Ejemplo_03_Vehiculo {
 		this.lpkm = lpkm;
 	}
 
-	public int getPasajeros() {
-		return pasajeros;
-	}
+	public int getPasajeros() { return pasajeros; }
+	public void setPasajeros(int pasajeros) { this.pasajeros = pasajeros; }
 
-	public void setPasajeros(int pasajeros) {
-		this.pasajeros = pasajeros;
-	}
+	public int getCapacidad() { return capacidad; }
+	public void setCapacidad(int capacidad) { this.capacidad = capacidad; }
 
-	public int getCapacidad() {
-		return capacidad;
-	}
+	public int getLpkm() { return lpkm; }
+	public void setLpkm(int lpkm) { this.lpkm = lpkm; }
 
-	public void setCapacidad(int capacidad) {
-		this.capacidad = capacidad;
-	}
-
-	public int getLpkm() {
-		return lpkm;
-	}
-
-	public void setLpkm(int lpkm) {
-		this.lpkm = lpkm;
-	}
-
-	public int rango() {
-		return this.capacidad * this.lpkm;
-	}
+	public int rango() { return this.capacidad * this.lpkm; }
 }
