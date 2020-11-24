0eca391348c80cc62f6a1bfc1b85f59fa0990468
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-24 13:33:48 +0100

    Update Ejemplo_03.java

--- AD/UT1/src/Ejemplos/Ejemplo_03.java
+++ AD/UT1/src/Ejemplos/Ejemplo_03.java
@@ -3,8 +3,11 @@ package Ejemplos;
 public class Ejemplo_03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Ejemplo_03_Vehiculo minivan = new Ejemplo_03_Vehiculo(9, 50, 18);
+		
+		System.out.println("La minivan puede llevar " + minivan.getPasajeros() + " "
+				+ "pasajeros con un rango de " + minivan.rango() + " "
+				+ "Kilómetros con el depósito lleno");
 	}
-
 }
