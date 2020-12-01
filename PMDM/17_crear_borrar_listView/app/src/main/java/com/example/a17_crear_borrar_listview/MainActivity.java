2d7e130726c60068cc8de249701bdc704d42a49a
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-01 10:36:21 +0100

    Update MainActivity.java

--- PMDM/17_crear_borrar_listView/app/src/main/java/com/example/a17_crear_borrar_listview/MainActivity.java
+++ PMDM/17_crear_borrar_listView/app/src/main/java/com/example/a17_crear_borrar_listview/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.DialogInterface;
+import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText textoAgregar;
     private ListView lista;
     private ArrayList<String> arrLista;
+    private ArrayList<String> arrayListDup = new ArrayList<>();
     private ArrayAdapter adapter;
 
     /**
@@ -59,62 +63,90 @@ public class MainActivity extends AppCompatActivity {
 
         // Añade un evento a los elementos de la lista
         lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-        @Override
-        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
-            AlertDialog.Builder diag = new AlertDialog.Builder(MainActivity.this);
-            diag.setCancelable(false);
-            diag.setTitle(R.string.titleRemoveDialog);
-            diag.setMessage(getResources().getString(R.string.msgRemoveDialog));
-            diag.setNegativeButton(getResources().getString(R.string.aceptar), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.cancel();
-                }
-            });
-            diag.setPositiveButton(getResources().getString(R.string.cancelar), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    arrLista.remove(position);
-                    adapter.notifyDataSetChanged();
-                }
-            });
-            diag.show();
+                // Se duplica el array de elementos
+                arrayListDup.clear();
+                arrayListDup.addAll(arrLista);
 
-            return false;
-        }
-    });
-}
+                String selItem = arrLista.get(position);
+
+                // Se crea el dialogo para eliminar el elemento
+                AlertDialog.Builder diag = new AlertDialog.Builder(MainActivity.this);
+                diag.setCancelable(false);
+                diag.setTitle(R.string.titleRemoveDialog);
+                diag.setMessage(getResources().getString(R.string.msgRemoveDialog));
+                diag.setNegativeButton(getResources().getString(R.string.cancelar), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                diag.setPositiveButton(getResources().getString(R.string.aceptar), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        arrLista.remove(position);
+                        adapter.notifyDataSetChanged();
+
+                        // Se crea un snackbar para deshacer el eliminado
+                        Snackbar snc = Snackbar.make(view, "Se ha eliminado " + selItem, Snackbar.LENGTH_LONG);
+                        snc.setAction("Deshacer", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                arrLista.clear();
+                                arrLista.addAll(arrayListDup);
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+                        snc.show();
+                    }
+                });
+                diag.show();
+
+                return false;
+            }
+        });
+    }
 
     /**
      * Funcion que se ejecuta al pulsar el boton agregar elemento
      * @param view
      */
-    public void agregarElemento(View view) {
+    public void agregarElemento(View view)  {
 
         textoAgregar = findViewById(R.id.textoAgregar);
         String textoAgregarText = textoAgregar.getText().toString();
 
         if (!textoAgregarText.matches("")) {
 
+            // Oculta el teclado al añadir un elemento
+            InputMethodManager imm = (InputMethodManager) this.getSystemService(Activity.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+            // Se duplica el array de elementos
+            arrayListDup.clear();
+            arrayListDup.addAll(arrLista);
+
+            String selItem = textoAgregar.getText().toString();
+
+            // Se añade el elemento a la lista, se recarga la vista y se borra el texto del textbox
             arrLista.add(textoAgregarText);
             adapter.notifyDataSetChanged();
             textoAgregar.setText("");
 
-            /*
             // Se crea el SnackBar
-            Snackbar snc = Snackbar.make(view, textoAgregar.getText() + " Agregado", Snackbar.LENGTH_LONG);
-
-            // Se le asigna una accion al SnackBar
-            snc.setAction("Undo", new View.OnClickListener() {
+            Snackbar snc = Snackbar.make(view, "Se ha creado " + selItem, Snackbar.LENGTH_LONG);
+            snc.setAction("Deshacer", new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "Accion", Toast.LENGTH_SHORT).show();
+                    arrLista.clear();
+                    arrLista.addAll(arrayListDup);
+                    adapter.notifyDataSetChanged();
                 }
             });
-
-            // Se muestra el SnackBar
-            snc.show(); */
-        } else { Toast.makeText(MainActivity.this, "Tiene que indicar un nombre", Toast.LENGTH_SHORT).show(); }
+            snc.show();
+        }
+        else { Toast.makeText(MainActivity.this, "Tiene que indicar un nombre", Toast.LENGTH_SHORT).show(); }
     }
 }
