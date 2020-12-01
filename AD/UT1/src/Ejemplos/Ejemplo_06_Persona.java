ac7dd4a1d10d54663c9f403bedfd30c9434cff9e
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-01 13:02:24 +0100

    Create Ejemplo_06_Persona.java

---
+++ AD/UT1/src/Ejemplos/Ejemplo_06_Persona.java
@@ -0,0 +1,31 @@
+package Ejemplos;
+
+import java.io.Serializable;
+
+public class Ejemplo_06_Persona {
+	
+	private String nombre;
+	private int edad;
+	
+	public Ejemplo_06_Persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+}
