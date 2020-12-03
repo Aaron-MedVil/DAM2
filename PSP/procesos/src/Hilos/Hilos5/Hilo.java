03bb6b230dde9ed1eaa51f3c2f21a9553868e2da
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-03 13:59:12 +0100

    Update Hilo.java

--- PSP/procesos/src/Hilos/Hilos5/Hilo.java
+++ PSP/procesos/src/Hilos/Hilos5/Hilo.java
@@ -4,6 +4,8 @@ public class Hilo extends Thread {
 
 	public void run() {
 		
-		while (true) { System.out.println("Hola, soy un hilo activo"); }
+		while (!interrupted()) { System.out.println("Hola, soy un hilo activo"); }
+		
+		System.out.println("El hilo ha muerto");
 	}
 }
