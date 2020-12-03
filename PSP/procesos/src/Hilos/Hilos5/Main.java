3b945fcfe8fa7e6830d3fcb9843d37398ac69fd8
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-03 13:50:04 +0100

    Update Main.java

--- PSP/procesos/src/Hilos/Hilos5/Main.java
+++ PSP/procesos/src/Hilos/Hilos5/Main.java
@@ -3,8 +3,9 @@ package Hilos.Hilos5;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Hilo h = new Hilo();
+		h.start();
+		h.interrupt();
 	}
-
 }
