bfd44e7ce67e71750ab44328ddcb8ae0b94a6655
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-03 13:59:20 +0100

    Update Main.java

--- PSP/procesos/src/Hilos/Hilos5/Main.java
+++ PSP/procesos/src/Hilos/Hilos5/Main.java
@@ -2,10 +2,15 @@ package Hilos.Hilos5;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		Hilo h = new Hilo();
 		h.start();
+		
+		Thread.sleep(2000);
 		h.interrupt();
+		
+		// if (h.isInterrupted() == true) { System.out.println("El hilo ha muerto"); }
+		// else { System.out.println("Hilo en ejecucion"); }
 	}
 }
