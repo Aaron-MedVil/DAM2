88d5226e98684a1db7bc87a3277a2d3b538bac55
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-09 08:48:40 +0100

    Update Main.java

--- PSP/procesos/src/Hilos/HIlos7/Main.java
+++ PSP/procesos/src/Hilos/HIlos7/Main.java
@@ -2,9 +2,26 @@ package Hilos.HIlos7;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		
+		Hilo h = new Hilo();
+		
+		// Ejecutamos el hilo
+		h.start();
+		
+		Thread.sleep(2000);
+		
+		// Suspendemos el hilo
+		h.suspender();
+		
+		Thread.sleep(2000);
+		
+		// Reanudamos el hilo
+		h.reanudar();
+		
+		Thread.sleep(2000);
+		
+		// Interrumpimos el hilo
+		h.interrupt();
 	}
-
 }
