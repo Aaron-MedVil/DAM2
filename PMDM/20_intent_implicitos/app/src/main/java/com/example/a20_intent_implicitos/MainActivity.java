9bade57b881768191f995364ceaa0c790755647c
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-09 13:35:51 +0100

    Update MainActivity.java

--- PMDM/20_intent_implicitos/app/src/main/java/com/example/a20_intent_implicitos/MainActivity.java
+++ PMDM/20_intent_implicitos/app/src/main/java/com/example/a20_intent_implicitos/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.a20_intent_implicitos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Metodo que se ejecuta al crearse la aplicacion
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Metodo para crear una alarma
+     * @param message
+     * @param hour
+     * @param minutes
+     */
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * Metodo que se ejecuta cuando se pulsa el boton para crear la alarma
+     * @param view
+     */
+    public void crearAlarma(View view) {
     }
 }
