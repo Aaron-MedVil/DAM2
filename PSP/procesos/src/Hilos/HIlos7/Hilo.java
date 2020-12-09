cd26d57b4a1446998d2e4ff7199904eb88ddaeaf
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-09 08:42:40 +0100

    Update Hilo.java

--- PSP/procesos/src/Hilos/HIlos7/Hilo.java
+++ PSP/procesos/src/Hilos/HIlos7/Hilo.java
@@ -1,5 +1,34 @@
 package Hilos.HIlos7;
 
-public class Hilo {
+public class Hilo extends Thread {
 
+	private boolean suspendido;
+	
+	/**
+	 * Suspende la ejecucion del hilo
+	 */
+	public synchronized void suspender() {
+		System.out.println("Suspendiendo el hilo");
+		suspendido = true;
+	}
+	
+	/**
+	 * Reanuda la ejecucion del hilo
+	 */
+	public synchronized void reanudar() {
+		System.out.println("Reanudando el hilo");
+		suspendido = false;
+		notifyAll();
+	}
+	
+	/**
+	 * Codigo que ejecuta el hilo cuando esta en suspension
+	 * @throws InterruptedException 
+	 */
+	public synchronized void enSuspension() throws InterruptedException {
+		while(suspendido) {
+			wait();
+			System.out.println("Hilo en espera");
+		}
+	}
 }
