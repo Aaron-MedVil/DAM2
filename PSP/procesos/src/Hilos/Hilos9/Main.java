4540d77b71f217b50cf0e9eac6363383f382f35c
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-10 13:25:00 +0100

    Update Main.java

--- PSP/procesos/src/Hilos/Hilos9/Main.java
+++ PSP/procesos/src/Hilos/Hilos9/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Hilo h = new Hilo();
-		Hilo h1 = new Hilo();
-		
+		Hilo h = new Hilo(), h1 = new Hilo();
 		h.start(); h1.start();
 	}
 }
