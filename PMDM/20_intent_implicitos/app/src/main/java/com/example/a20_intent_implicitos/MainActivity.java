3b1f5108571be949658603829f92702952fece51
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-10 12:39:15 +0100

    Update MainActivity.java

--- PMDM/20_intent_implicitos/app/src/main/java/com/example/a20_intent_implicitos/MainActivity.java
+++ PMDM/20_intent_implicitos/app/src/main/java/com/example/a20_intent_implicitos/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a20_intent_implicitos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.AlarmClock;
 import android.view.View;
@@ -36,11 +37,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * Metodo para realizar una llamada
+     * @param phoneNumber
+     */
+    public void dialPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    /**
      * Metodo que se ejecuta cuando se pulsa el boton para crear la alarma
      * @param view
      */
     public void crearAlarma(View view) {
 
         createAlarm("Pruebas", 14, 00);
+    }
+
+    /**
+     * Metodo que se ejecuta cuando se pulsa el boton para realizar una llamada
+     * @param view
+     */
+    public void realizarLlamada(View view) {
+
+        dialPhoneNumber("652693979");
     }
 }
