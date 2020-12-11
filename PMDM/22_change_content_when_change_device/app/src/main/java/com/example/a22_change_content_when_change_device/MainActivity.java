78441ef181e902ddcd97091c861abddfaa05ab81
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-11 09:00:11 +0100

    Create 22_change_content_when_change_device

---
+++ PMDM/22_change_content_when_change_device/app/src/main/java/com/example/a22_change_content_when_change_device/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a22_change_content_when_change_device;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
