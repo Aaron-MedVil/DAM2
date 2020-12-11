2a0358e57b58af82ba5ebd698551a7220dd2964a
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-11 08:35:33 +0100

    Create Activity_gestor_alarmas.java

---
+++ PMDM/20_intent_implicitos/app/src/main/java/com/example/a20_intent_implicitos/Activity_gestor_alarmas.java
@@ -0,0 +1,14 @@
+package com.example.a20_intent_implicitos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_gestor_alarmas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestor_alarmas);
+    }
+}
