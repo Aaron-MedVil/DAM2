2a570d96cf1f994dda8266b620de5006c9f23fb3
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-11 08:42:11 +0100

    Create 21_landscape_and_portrait

---
+++ PMDM/21_landscape_and_portrait/app/src/main/java/com/example/a21_landscape_and_portrait/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a21_landscape_and_portrait;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
