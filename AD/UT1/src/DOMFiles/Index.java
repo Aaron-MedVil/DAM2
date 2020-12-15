451cb5e206da465a7ee5cfd8fd088d6b75de9043
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-15 12:33:14 +0100

    Create Index.java

---
+++ AD/UT1/src/DOMFiles/Index.java
@@ -0,0 +1,16 @@
+package DOMFiles;
+
+public class Index {
+
+	/**
+	 * Metodo principal que invoca los metodos para crear el fichero XML
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		DomGenerator dg = new DomGenerator();
+		dg.generarDocumento();
+		dg.generarXML();
+	}
+}
