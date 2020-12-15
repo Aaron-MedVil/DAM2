e287a50132fc832a94f4c133b3dfdb14f1aa1e0e
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-15 10:21:20 +0100

    Create 24_recycler_view_fragments_com

---
+++ PMDM/24_recycler_view_fragments_com/app/src/main/java/com/example/a24_recycler_view_fragments_com/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a24_recycler_view_fragments_com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
