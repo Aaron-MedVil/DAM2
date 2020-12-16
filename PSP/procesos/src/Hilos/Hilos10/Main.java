f1459fa4db51272bd7abd09670efb15e5527f9e1
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-16 16:04:37 +0100

    Update Main.java

--- PSP/procesos/src/Hilos/Hilos10/Main.java
+++ PSP/procesos/src/Hilos/Hilos10/Main.java
@@ -2,9 +2,23 @@ package Hilos.Hilos10;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		
+		final int t = 200;
+		
+		// Instancias de los hilos
+		Hilo h1 = new Hilo(), h2 = new Hilo(), h3 = new Hilo();
+		
+		// Propiedades de los hilos
+		h1.setPriority(3); h2.setPriority(1); h3.setPriority(2);
+		
+		// Inicio de los hilos
+		h1.start(); h2.start(); h3.start();
+		
+		// Tiempo que estara activo el hilo
+		Thread.sleep(t);
+		
+		// Detiene los hilos
+		h1.pararHilo(); h2.pararHilo(); h3.pararHilo();
 	}
-
 }
