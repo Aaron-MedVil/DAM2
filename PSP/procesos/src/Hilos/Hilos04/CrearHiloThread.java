4cd3bb7e09cdb07930350060c2a2992c34992a7a
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-16 08:27:12 +0100

    Create CrearHiloThread.java

---
+++ PSP/procesos/src/Hilos/Hilos04/CrearHiloThread.java
@@ -0,0 +1,12 @@
+package Hilos.Hilos04;
+
+public class CrearHiloThread extends Thread {
+
+	private int id;
+
+	public CrearHiloThread(int id) { this.id = id; }
+
+	public void run() {
+		for (int i = 0; i < 100; i++) { System.out.println("[T]Ejecutándose el hilo " + id + " Contador: " + i); }
+	}
+}
