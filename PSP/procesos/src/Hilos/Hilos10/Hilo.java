25d1a2bd61608a1311fc9838c9a9133b41a89e0b
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-16 16:04:34 +0100

    Update Hilo.java

--- PSP/procesos/src/Hilos/Hilos10/Hilo.java
+++ PSP/procesos/src/Hilos/Hilos10/Hilo.java
@@ -1,5 +1,26 @@
 package Hilos.Hilos10;
 
-public class Hilo {
+public class Hilo extends Thread {
+	
+	private boolean suspendido = false;
+	private int cont = 0;
 
+	/**
+	 * Metodo que ejecuta el hilo
+	 */
+	public void run() {
+		
+		while (!suspendido) { System.out.printf("Hilo %s: cont %d %n", this.getName(), ++cont); }
+	}
+	
+	/**
+	 * Devuelve el contador del hilo
+	 * @return
+	 */
+	public int getContadorHilo() { return cont;	}
+	
+	/**
+	 * Metodo para detener la ejecucuon del hilo
+	 */
+	public synchronized void pararHilo() { suspendido = true; }
 }
