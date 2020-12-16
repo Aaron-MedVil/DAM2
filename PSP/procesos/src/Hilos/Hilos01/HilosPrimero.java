a3c49401c5a8a70b8bba669b0d1ad771333497fc
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-16 08:26:24 +0100

    Create HilosPrimero.java

---
+++ PSP/procesos/src/Hilos/Hilos01/HilosPrimero.java
@@ -0,0 +1,12 @@
+package Hilos.Hilos01;
+
+public class HilosPrimero extends Thread {
+	
+	public void run() {
+		
+		for (int i = 0; i < 5; i++) {
+			
+			System.out.println("Dentro del hilo " + i);
+		}
+	}
+}
