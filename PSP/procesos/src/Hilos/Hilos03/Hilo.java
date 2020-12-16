8213c709877804885d3a4b6e18a5b3737007f923
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-16 08:27:05 +0100

    Create Hilo.java

---
+++ PSP/procesos/src/Hilos/Hilos03/Hilo.java
@@ -0,0 +1,24 @@
+// Para ejecutarlo primero compilamos desde la carpeta src con el comando "javac Hilos/Hilos3/Hilo.java"
+// Despues ejecutamos desde la carpeta src con el comando "java Hilos/Hilos3/Hilo"
+
+package Hilos.Hilos03;
+
+public class Hilo {
+
+	public static void main(String[] args) {
+		
+		CrearHilo hilo = null;
+		
+		for (int i = 1; i <= 3; i++) {
+			
+			hilo = new CrearHilo();
+			hilo.setName("hilo"+i);
+			hilo.setPriority(1);
+			hilo.start();
+			
+			System.out.println("Info del hilo " + hilo.getName() + ":\n" + hilo.toString() + "\n");
+		}
+		
+		System.out.println(" --- Tres hilos creados --- ");
+	}
+}
