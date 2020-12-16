f120975aa26b30ae2af9990fbc4b9413171ce593
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-16 08:28:23 +0100

    Create Hilo.java

---
+++ PSP/procesos/src/Hilos/Hilos09/Hilo.java
@@ -0,0 +1,21 @@
+package Hilos.Hilos09;
+
+public class Hilo extends Thread implements Runnable {
+
+	/**
+	 * Funcion quese ejecuta el hilo
+	 */
+	@Override
+	public void run() {
+		
+		int n = 0;
+		
+		while(n != 2) {
+			
+			System.out.printf("Hilo %s random %d %n", this.getName(), n);
+			n = (int) (Math.random() * 10);
+		}
+		
+		System.out.printf("Fin del hilo %s %n", this.getName());
+	}
+}
