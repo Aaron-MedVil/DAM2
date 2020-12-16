f1bab05a94bf5f6bf72fea163a30e27ff9ce7762
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-16 08:22:36 +0100

    Update index.java

--- AD/UT1/src/SAXFiles/index.java
+++ AD/UT1/src/SAXFiles/index.java
@@ -8,6 +8,7 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 public class index {
 
@@ -21,10 +22,10 @@ public class index {
 	public static void main(String[] args) throws FileNotFoundException, IOException, SAXException {
 		
 		@SuppressWarnings("deprecation")
-		XMLReader procesadorXML;
+		XMLReader procesadorXML = XMLReaderFactory.createXMLReader();
 		GestionContenido gestor = new GestionContenido();
 		procesadorXML.setContentHandler(gestor);
-		InputSource fileXML = new InputSource("./Resources/alumnos-min.xml");
+		InputSource fileXML = new InputSource("./Resources/alumnos.xml");
 		procesadorXML.parse(fileXML);
 	}
 }
@@ -45,14 +46,14 @@ class GestionContenido extends DefaultHandler {
 	 * Metodo que se ejecuta al iniciar el documento
 	 */
 	public void startDocument() {
-		System.out.println("Comienzo del documento XML");
+		System.out.println(" ==================== Comienzo del documento XML ==================== \n");
 	}
 	
 	/**
 	 * Metodo que se ejecuta al terminar el fichero XML
 	 */
 	public void endDocument() {
-		System.out.println("Fin del documento XML");
+		System.out.println("\n ==================== Fin del documento XML ==================== ");
 	}
 	
 	/**
@@ -64,6 +65,9 @@ class GestionContenido extends DefaultHandler {
 	 */
 	public void startElement(String url, String nombre, String nombreC, Attributes atts) {
 		System.out.printf("\tInicio del Elemento: %s %n", nombre);
+		for (int i = 0; i < atts.getLength(); i++) {
+            System.out.printf("\t\tAtributo %s: %s %n", atts.getQName(i), atts.getValue(i));
+        }
 	}
 	
 	/**
@@ -73,11 +77,19 @@ class GestionContenido extends DefaultHandler {
 	 * @param nombreC
 	 * @param atts
 	 */
-	public void endElement(String url, String nombre, String nombreC, Attributes atts) {
+	public void endElement(String url, String nombre, String nombreC) {
 		System.out.printf("\tFin del Elemento: %s %n", nombre);
 	}
 	
+	/**
+	 * Metodo que lee el contenido de los elementos
+	 * @param ch
+	 * @param inicio
+	 * @param longitud
+	 */
 	public void characters(char[] ch, int inicio, int longitud) throws SAXException {
-		
+		String car = new String(ch, inicio, longitud);
+		car = car.replaceAll("[\t\n]", "");
+		if (!car.trim().isEmpty()) { System.out.printf("\t\tCaracteres: %s %n", car); }
 	}
 }
