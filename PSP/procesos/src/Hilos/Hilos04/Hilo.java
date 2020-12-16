377f2c5e29513dbb297d2c224536d6bbff73de8c
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-16 08:27:16 +0100

    Create Hilo.java

---
+++ PSP/procesos/src/Hilos/Hilos04/Hilo.java
@@ -0,0 +1,15 @@
+package Hilos.Hilos04;
+
+public class Hilo {
+
+	public static void main(String[] args) {
+		
+		CrearHiloThread hilo1 = new CrearHiloThread(1);
+        hilo1.start();
+        
+        Thread hilo2 = new Thread(new CrearHiloRunnable(2));
+        hilo2.start();
+        
+        for(int i = 0; i<100;i++) { System.out.println("Ejecutándose hilo MAIN "+i); }
+	}
+}
