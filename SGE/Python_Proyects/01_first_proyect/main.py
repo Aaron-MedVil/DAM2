d393b76dc1229b3fa1b8e4f27831a89f0c10ff8f
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-17 09:36:21 +0100

    Update main.py

--- SGE/Python_Proyects/01_first_proyect/main.py
+++ SGE/Python_Proyects/01_first_proyect/main.py
@@ -4,8 +4,11 @@ entero = 1
 real = 1.2
 division = 5/3
 complejo = 2 + 1j
+hola = "hola"
+mundo = "mundo"
 
 print("Entero: ", entero)
 print("Real: ", real)
 print("Division: ", division)
 print("Complejo: ", complejo)
+print("String: ", hola, mundo)
