3bd29df42cd619093d55222b5feafac127d70b21
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-17 13:54:38 +0100

    Update RecyclerFragment.java

--- PMDM/24_recycler_view_fragments_com/app/src/main/java/com/example/a24_recycler_view_fragments_com/RecyclerFragment.java
+++ PMDM/24_recycler_view_fragments_com/app/src/main/java/com/example/a24_recycler_view_fragments_com/RecyclerFragment.java
@@ -3,10 +3,14 @@ package com.example.a24_recycler_view_fragments_com;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,25 +19,54 @@ import android.view.ViewGroup;
  */
 public class RecyclerFragment extends Fragment {
 
+    ArrayList<Producto> listProducto;
+    RecyclerView rv;
+
     public RecyclerFragment() {}
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
+     * Use this factory method to create a new instance of this fragment using the provided parameters.
      * @return A new instance of fragment RecyclerFragment.
      */
-    public static RecyclerFragment newInstance(String param1, String param2) {
+    public static RecyclerFragment newInstance() {
         return new RecyclerFragment();
     }
 
+    /**
+     * Metodo que se ejecuta al crear el fragmento
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    /**
+     * Metodo que se ejecuta cuando se crea la vista del fragmento
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View v = inflater.inflate(R.layout.fragment_recycler, container, false);
+
+        listProducto = new ArrayList<Producto>();
+        rv = v.findViewById(R.id.idContFragmentRecycler);
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        llenarLista();
+
         return v;
+    }
+
+    /**
+     * Añade elementos a la lista que mostraremos
+     */
+    private void llenarLista() {
+
+        listProducto.add(new Producto());
     }
 }
