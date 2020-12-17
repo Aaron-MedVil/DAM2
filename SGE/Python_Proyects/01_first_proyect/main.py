b90ef483387dc3b4afa5169482d640d0085b1f79
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-17 10:01:45 +0100

    Update main.py

--- SGE/Python_Proyects/01_first_proyect/main.py
+++ SGE/Python_Proyects/01_first_proyect/main.py
@@ -1,14 +1,26 @@
 # Declaracion de caracteres
 
+## Numericos
 entero = 1
 real = 1.2
 division = 5/3
 complejo = 2 + 1j
+
+## Cadenas
 hola = "hola"
 mundo = "mundo"
+result = hola + " " + mundo # Iteracion de dos cadenas
+
+## Colecciones
+lista = [0, 1, 2] # Las listas son arrays que se pueden añadir y eliminar registros
+tupla = (0, 1, 2) # Las tuplas son arrays que nunca cambian
+diccionario = {0: "v0", 1: "v1"} # Objetos clave-valor
 
 print("Entero: ", entero)
 print("Real: ", real)
 print("Division: ", division)
 print("Complejo: ", complejo)
-print("String: ", hola, mundo)
+print("String: ", result)
+print("Lista: ", lista)
+print("Tupla: ", tupla)
+print("Diccionario", diccionario)
