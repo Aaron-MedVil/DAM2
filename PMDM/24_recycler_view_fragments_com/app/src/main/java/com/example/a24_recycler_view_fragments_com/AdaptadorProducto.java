8df497355049570e40ed15ad47665d05fe7779cc
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-17 13:27:14 +0100

    Update AdaptadorProducto.java

--- PMDM/24_recycler_view_fragments_com/app/src/main/java/com/example/a24_recycler_view_fragments_com/AdaptadorProducto.java
+++ PMDM/24_recycler_view_fragments_com/app/src/main/java/com/example/a24_recycler_view_fragments_com/AdaptadorProducto.java
@@ -1,21 +1,40 @@
 package com.example.a24_recycler_view_fragments_com;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class AdaptadorProducto extends RecyclerView.Adapter<AdaptadorProducto.ViewHolderProducto> {
 
+    ArrayList<Producto> listProducto;
+
+    /**
+     * Carga el layout personalizado que hemos creado en la vista
+     * @param parent
+     * @param viewType
+     * @return
+     */
     @NonNull @Override
-    public AdaptadorProducto.ViewHolderProducto onCreateViewHolder(@NonNull ViewGroup parent, int viewType) { return null; }
+    public ViewHolderProducto onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recycler, null, false);
+        return new ViewHolderProducto(v);
+    }
 
     @Override
-    public void onBindViewHolder(@NonNull AdaptadorProducto.ViewHolderProducto holder, int position) {}
+    public void onBindViewHolder(@NonNull ViewHolderProducto holder, int position) {
+
+        final Producto producto = listProducto.get(position);
+        String nomProd = producto.getNomProducto(), descProd = producto.getDescProducto();
+        int imgProd = producto.getImgProducto();
+    }
 
     @Override
-    public int getItemCount() { return 0; }
+    public int getItemCount() { return listProducto.size(); }
 
     public class ViewHolderProducto extends RecyclerView.ViewHolder {
         public ViewHolderProducto(@NonNull View itemView) {
