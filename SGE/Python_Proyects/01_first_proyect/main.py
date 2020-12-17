ae2904e6c755b12c1ebf195036f3b31aa181fd3b
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-17 09:22:12 +0100

    Create main.py

---
+++ SGE/Python_Proyects/01_first_proyect/main.py
@@ -0,0 +1,11 @@
+# Declaracion de caracteres
+
+entero = 1
+real = 1.2
+division = 5/3
+complejo = 2 + 1j
+
+print("Entero: ", entero)
+print("Real: ", real)
+print("Division: ", division)
+print("Complejo: ", complejo)
