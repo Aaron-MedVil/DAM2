a9661f07f39b5e5a31761d4dfcc5de0665e1de33
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-18 19:18:32 +0100

    Create Fabrica

---
+++ DI/Fabrica/Fabrica/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fabrica {
+
+    public partial class MainWindow : Window {
+
+        private Modelo.FabricaSql fabricaSql = new Modelo.FabricaSql();
+        private DataTable tablaMateriasPrimas = null;
+        private DataRow[] filas = null;
+
+        /// <summary>Funcion que carga los componentes de la pagina</summary>
+        public MainWindow() => InitializeComponent();
+
+        /// <summary>Carga los datos iniciales</summary>
+        /// <param name="sender">Objeto que lanza la accion</param>
+        /// <param name="e">Parametros del evento</param>
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+
+            Dictionary<String, dynamic> resultConn = new Dictionary<string, dynamic>();
+            resultConn = fabricaSql.checkConn();
+
+            if ((bool)resultConn["status"]) {
+
+                Dictionary<String, dynamic> result = new Dictionary<string, dynamic>();
+                result = fabricaSql.cargaDatosMateriasPrimas(null);
+
+                if ((bool)result["status"]) {
+
+                    tablaMateriasPrimas = (DataTable)result["data"];
+                    filas = tablaMateriasPrimas.Select("id > 0");
+                    visualizaRegistro(filas[0]);
+                } else { MessageBox.Show(result["msg"]); }
+            } else { MessageBox.Show(resultConn["msg"]); }
+        }
+
+        /// <summary>Visualiza los datos en la interfaz</summary>
+        /// <param name="registro">Registros</param>
+        public void visualizaRegistro(DataRow registro) {
+
+            cId.Text          = registro["id"].ToString();
+            cDescripcion.Text = registro["descripcion"].ToString();
+            cProveedor.Text   = registro["idProveedor"].ToString();
+            cFamilia.Text     = registro["idFamilia"].ToString();
+            cStock.Text       = registro["stock"].ToString();
+            cStockMinimo.Text = registro["stockMinimo"].ToString();
+            cPrecioCoste.Text = registro["precioCoste"].ToString();
+            cTImagen.Text     = registro["rutaImagen"].ToString();
+
+            cDescripcion.Focus();
+        }
+
+        private void SiguienteRegistro(object sender, RoutedEventArgs e) {}
+
+        private void AnterioreRegistro(object sender, RoutedEventArgs e) {}
+
+        private void PrimerRegistro(object sender, RoutedEventArgs e) {}
+
+        private void UltimoRegistro(object sender, RoutedEventArgs e) {}
+
+        private void NuevoRegistro(object sender, RoutedEventArgs e) {}
+
+        private void GrabarRegistro(object sender, RoutedEventArgs e) {}
+
+        private void BorrarRegistro(object sender, RoutedEventArgs e) {}
+
+        private void BuscarRegistro(object sender, RoutedEventArgs e) {}
+
+        private void BuscarRegistroSiguiente(object sender, MouseButtonEventArgs e) {}
+    }
+}
---
+++ DI/Fabrica/Fabrica/Modelo/FabricaSql.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace Fabrica.Modelo {
+
+    class FabricaSql {
+
+        private String connStr = ConfigurationManager.ConnectionStrings["connFabricaSql"].ConnectionString;
+        private SqlConnection conn = null;
+        private SqlDataAdapter adp = null;
+        private SqlCommand cmd = null;
+        private SqlDataReader reader = null;
+
+        private Dictionary<String, dynamic> retornos = null;
+
+        /// <summary>Comprueba la conexion a la base de datos</summary>
+        /// <returns type="Dictionary">Informacion de la conexion</returns>
+        public Dictionary<String, dynamic> checkConn() {
+
+            retornos = new Dictionary<String, dynamic>();
+
+            bool isConn = false;
+            string msg = "Error al conectar a la base de datos";
+
+            try {
+
+                conn = new SqlConnection(connStr);
+                conn.Open();
+                isConn = true;
+                msg = "Conexión realizada correctamente";
+            }
+            catch (SqlException e) { msg = e.ToString(); }
+            finally { if (conn.State == ConnectionState.Open) { conn.Close(); } }
+
+            retornos.Add("status", isConn);
+            retornos.Add("msg", msg);
+
+            return retornos;
+        }
+
+        /// <summary></summary>
+        /// <param name="args">Argumentos para la sentencia</param>
+        /// <returns type="Dictionary">Datos de la consulta</returns>
+        public Dictionary<String, dynamic> cargaDatosMateriasPrimas(Dictionary<string, string>? args) {
+
+            DataTable dataTabla = new DataTable();
+
+            retornos = new Dictionary<string, dynamic>();
+            bool status = false;
+            string msg = "Error en la sentencia";
+
+            try {
+
+                // Crea la conexion y la abre
+                conn = new SqlConnection(connStr);
+                conn.Open();
+
+                // Crea la sentencia a la base de datos
+                cmd = new SqlCommand();
+                cmd.CommandText = "SELECT id, Descripcion, idProveedor, idFamilia, stock, stockMinimo, precioCoste, rutaImagen, imagen FROM MateriasPrimas";
+
+                // Carga los argumentos en la sentencia
+                if (args != null) {
+
+                    cmd.CommandText += " WHERE 1 = @def";
+
+                    foreach (var param in args) {
+
+                        cmd.CommandText += " AND " + param.Key + " = @" + param.Key;
+                        cmd.Parameters.AddWithValue("@" + param.Key, param.Value);
+                    }
+
+                    cmd.Parameters.AddWithValue("@def", 1);
+                }
+
+                // Conecta la sentencia y la conexion
+                cmd.Connection = conn;
+
+                // Ejecuta la sentencia
+                reader = cmd.ExecuteReader();
+
+                // Comprueba que se retornan datos
+                if (reader.HasRows) {
+
+                    // Carga los datos en la datatabla
+                    dataTabla.Load(reader);
+                    retornos.Add("data", dataTabla);
+
+                    status = true;
+                    msg = "Datos de la primera conexion";
+                } else { msg = "No se devolvieron datos"; }
+            }
+            catch (SqlException e) { msg = e.ToString(); }
+            finally { if (conn.State == ConnectionState.Open) { conn.Close(); } }
+
+            retornos.Add("status", status);
+            retornos.Add("msg", msg);
+            return retornos;
+        }
+    }
+}
