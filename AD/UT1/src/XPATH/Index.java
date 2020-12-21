a5a7e4ae5f8a41dd022543ce5565eb3e9accf80b
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-21 08:29:14 +0100

    Update Index.java

--- AD/UT1/src/XPATH/Index.java
+++ AD/UT1/src/XPATH/Index.java
@@ -1,10 +1,34 @@
 package XPATH;
 
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
 public class Index {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 
+		String xPathExpression = "/espacio/galaxia/estrella/planeta";
+
+		// Carga del documento xml
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document documento = builder.parse(new File("./Resources/galaxia.xml"));
+
+		// Preparación de xpath
+		XPath xpath = XPathFactory.newInstance().newXPath();
+
+		// Consultas
+		NodeList nodos = (NodeList) xpath.evaluate(xPathExpression, documento, XPathConstants.NODESET);
+		for (int i = 0; i < nodos.getLength(); i++) {
+			System.out.println(nodos.item(i).getNodeName() + " : " + nodos.item(i).getAttributes().getNamedItem("nombre"));
+		}
 	}
-
 }
