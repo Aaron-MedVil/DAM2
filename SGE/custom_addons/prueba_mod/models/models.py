45fec46229bbed90f05b75403fbda66e55ef36c0
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-11 13:15:23 +0100

    Create new odoo module

---
+++ SGE/custom_addons/prueba_mod/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class prueba_mod(models.Model):
+#     _name = 'prueba_mod.prueba_mod'
+#     _description = 'prueba_mod.prueba_mod'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
