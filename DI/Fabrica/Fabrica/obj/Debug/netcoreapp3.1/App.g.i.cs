799fa65f15eef6cef46abe1e98b7bc26a02e35f9
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-12 12:26:19 +0100

    Update Fabrica

--- DI/Fabrica/Fabrica/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ DI/Fabrica/Fabrica/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -61,7 +61,7 @@ namespace Fabrica {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Fabrica;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Fabrica;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
