51f52428c1747d79904b2f6e530ab95188a215a7
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-13 12:44:39 +0100

    Update 26_Manejo_Datos

--- DI/26_Manejo_Datos/26_Manejo_Datos/MainWindow.xaml.cs
+++ DI/26_Manejo_Datos/26_Manejo_Datos/MainWindow.xaml.cs
@@ -11,18 +11,27 @@ namespace _26_Manejo_Datos {
         /// <summary>Carga los datos iniciales de la aplicacion</summary>
         /// <param name="sender">Elemento que realiza la accion</param>
         /// <param name="e">Parametros de la accion</param>
-        private void Window_Loaded(object sender, RoutedEventArgs e) {
-
-            checkSession();
-        }
+        private void Window_Loaded(object sender, RoutedEventArgs e) => checkSession();
 
         /// <summary>Comprueba si existe una sesion activa y si no existe la crea</summary>
         private void checkSession() {
 
-            if (!Globals.sessionStatus) { MessageBox.Show("Iniciar sesion"); }
-            else { MessageBox.Show("Cargar Datos"); }
+            /*if (!Globals.sessionStatus) { MessageBox.Show("Iniciar sesion"); }
+            else { MessageBox.Show("Cargar Datos"); }*/
         }
 
-        private void btnLogout_Click(object sender, RoutedEventArgs e) => this.Close();
+        /// <summary>Sale de la aplicacion</summary>
+        /// <param name="sender">Elemento que realiza la accion</param>
+        /// <param name="e">Parametros de la accion</param>
+        private void btn_exit_Click(object sender, RoutedEventArgs e) => this.Close();
+
+
+        private void btn_home_Click(object sender, RoutedEventArgs e) { /* cont_pages */ }
+
+        private void btn_json_Click(object sender, RoutedEventArgs e) { /* cont_pages */ }
+
+        private void btn_db_Click(object sender, RoutedEventArgs e) { /* cont_pages */ }
+
+        private void btn_user_Click(object sender, RoutedEventArgs e) { /* cont_pages */ }
     }
 }
--- DI/26_Manejo_Datos/26_Manejo_Datos/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ DI/26_Manejo_Datos/26_Manejo_Datos/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -61,7 +61,7 @@ namespace _26_Manejo_Datos {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/26_Manejo_Datos;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/26_Manejo_Datos;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
