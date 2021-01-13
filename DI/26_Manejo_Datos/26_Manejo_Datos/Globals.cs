25627f13385a899c141036059ea7480790574dc9
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-13 11:26:25 +0100

    Create 26_Manejo_Datos

---
+++ DI/26_Manejo_Datos/26_Manejo_Datos/Globals.cs
@@ -0,0 +1,23 @@
+﻿namespace _26_Manejo_Datos {
+
+    class Globals {
+
+        private static string? _sessionId = null, _sessionName = null;
+        private static bool _sessionStatus = false;
+
+        public static string sessionId {
+            get => _sessionId;
+            set => _sessionId = value;
+        }
+        
+        public static string sessionName {
+            get => _sessionName;
+            set => _sessionName = value;
+        }
+
+        public static bool sessionStatus {
+            get => _sessionStatus;
+            set => _sessionStatus = value;
+        }
+    }
+}
---
+++ DI/26_Manejo_Datos/26_Manejo_Datos/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace _26_Manejo_Datos {
+
+    public partial class MainWindow : Window {
+
+        /// <summary>Carga los componentes graficos de la aplicacion</summary>
+        public MainWindow() => InitializeComponent();
+
+        /// <summary>Carga los datos iniciales de la aplicacion</summary>
+        /// <param name="sender">Elemento que realiza la accion</param>
+        /// <param name="e">Parametros de la accion</param>
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+
+            checkSession();
+        }
+
+        /// <summary>Comprueba si existe una sesion activa y si no existe la crea</summary>
+        private void checkSession() {
+
+            if (!Globals.sessionStatus) { MessageBox.Show("Iniciar sesion"); }
+            else { MessageBox.Show("Cargar Datos"); }
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e) => this.Close();
+    }
+}
