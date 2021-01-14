2dd95af886ab533fcffcdcf4f559b83ab2d78896
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-14 13:19:23 +0100

    Create Producto.java

---
+++ PMDM/26_DbConn/app/src/main/java/com/example/a26_dbconn/Producto.java
@@ -0,0 +1,20 @@
+package com.example.a26_dbconn;
+
+public class Producto {
+
+    private int codigo;
+    private String nombre, descripcion;
+    private double precio;
+
+    public int getCodigo() { return codigo; }
+    public void setCodigo(int codigo) { this.codigo = codigo; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    public double getPrecio() { return precio; }
+    public void setPrecio(double precio) { this.precio = precio; }
+}
