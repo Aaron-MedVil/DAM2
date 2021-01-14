df991fa5d3d3b2ddcd72e9749707f53ada93544d
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-14 11:30:41 +0100

    Update models.py

--- SGE/custom_addons/brands/models/models.py
+++ SGE/custom_addons/brands/models/models.py
@@ -5,3 +5,8 @@ from odoo import models, fields, api
 class CustomBrand(models.Model):
     _inherit = 'product.template'
     brand = fields.Selection([("N", "Nike"), ("A", "Adidas")], string="Brand")
+
+# class Brand(models.Model):
+#    _name = 'brand.template'
+#    _description = 'brand.template'
+#    name = fields.Char(string='Brand')
