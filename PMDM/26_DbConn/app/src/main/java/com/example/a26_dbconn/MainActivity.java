10efd00e1431190f5d9e07d049f8d44a3311ccbc
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-14 13:19:20 +0100

    Update MainActivity.java

--- PMDM/26_DbConn/app/src/main/java/com/example/a26_dbconn/MainActivity.java
+++ PMDM/26_DbConn/app/src/main/java/com/example/a26_dbconn/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.a26_dbconn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etCod, etNom, etDesc, etPvp;
+    Button btnInsertar, btnBuscar, btnModificar, btnEliminar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etCod = findViewById(R.id.etCod);
+        etNom = findViewById(R.id.etNom);
+        etDesc = findViewById(R.id.etDesc);
+        etPvp = findViewById(R.id.etPvp);
     }
 }
