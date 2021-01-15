321bd635fcb234431265df94e31361ccb97eedee
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-15 09:42:13 +0100

    Update ProductoDB.java

--- PMDM/26_DbConn/app/src/main/java/com/example/a26_dbconn/ProductoDB.java
+++ PMDM/26_DbConn/app/src/main/java/com/example/a26_dbconn/ProductoDB.java
@@ -2,6 +2,7 @@ package com.example.a26_dbconn;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 public class ProductoDB {
 
@@ -18,5 +19,25 @@ public class ProductoDB {
         // Creamos la conexion a la base de datos
         SQLiteDatabase db = conn.getWritableDatabase();
         return db;
+    }
+
+    public int InsertaProducto(Context context, Producto prod) {
+
+        int res = 0;
+
+        String sql = "INSERT INTO productos (idProd, nomProd, descProd, pvpProd)" +
+                "VALUES ( " + prod.getCodigo() + " , '" + prod.getNombre() + "', '" + prod.getDescripcion() + "', " + prod.getPrecio() + ")";
+
+        SQLiteDatabase db = this.getConn(context);
+
+        try {
+
+            db.execSQL(sql);
+            res = 1;
+        }
+        catch(Exception e) { Log.e("Error", e.toString()); }
+        finally { db.close(); }
+
+        return res;
     }
 }
