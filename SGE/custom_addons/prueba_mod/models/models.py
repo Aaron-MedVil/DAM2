5e3261ae436631cebcf8b91d226faa7170dabde6
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-18 13:34:41 +0100

    Update models.py

--- SGE/custom_addons/prueba_mod/models/models.py
+++ SGE/custom_addons/prueba_mod/models/models.py
@@ -6,8 +6,8 @@ from odoo import models, fields, api
 class prueba_mod(models.Model):
     _name = 'prueba_mod.prueba_mod'
     _description = 'prueba_mod.prueba_mod'
+#    name = fields.Char(string  = "Brand")
 
-    name = fields.Char(string  = "Brand")
 #     name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
