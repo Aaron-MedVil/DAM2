b7845bdbeddaa9d8cef4cdca91c25d59c0947798
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-19 08:58:33 +0100

    Update ProductoDB.java

--- PMDM/26_DbConn/app/src/main/java/com/example/a26_dbconn/ProductoDB.java
+++ PMDM/26_DbConn/app/src/main/java/com/example/a26_dbconn/ProductoDB.java
@@ -21,12 +21,43 @@ public class ProductoDB {
         return db;
     }
 
+    /**
+     * Metodo que contiene la conexion a la base de datos para insertar un registro
+     * @param context
+     * @param prod
+     * @return
+     */
     public int InsertaProducto(Context context, Producto prod) {
 
         int res = 0;
 
         String sql = "INSERT INTO productos (idProd, nomProd, descProd, pvpProd)" +
                 "VALUES ( " + prod.getCodigo() + " , '" + prod.getNombre() + "', '" + prod.getDescripcion() + "', " + prod.getPrecio() + ")";
+
+        SQLiteDatabase db = this.getConn(context);
+
+        try {
+
+            db.execSQL(sql);
+            res = 1;
+        }
+        catch(Exception e) { Log.e("Error", e.toString()); }
+        finally { db.close(); }
+
+        return res;
+    }
+
+    /**
+     * Metodo que contiene la conexion a la base de datos para eliminar un registro
+     * @param context
+     * @param codProd
+     * @return
+     */
+    public int EliminarProducto(Context context, int codProd) {
+
+        int res = 0;
+
+        String sql = "DELETE FROM productos WHERE idProd = " + codProd;
 
         SQLiteDatabase db = this.getConn(context);
 
