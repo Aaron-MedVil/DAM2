c50ebd194a8031113f9c3c6b49f0fd5cf24c92dd
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-20 09:01:11 +0100

    Create Cola.java

---
+++ PSP/procesos/src/Hilos/Hilos12/Cola.java
@@ -0,0 +1,40 @@
+package Hilos.Hilos12;
+
+public class Cola {
+	
+	private int numero;
+	private boolean disponible = false;
+	
+	/**
+	 * Metodo para coger valores de la cola
+	 * @return
+	 */
+	public synchronized int get() {
+		
+		while(disponible == false) {
+			try { wait(); }
+			catch (InterruptedException e) {}
+		}
+		
+		disponible = false;
+		notifyAll();
+		return numero;
+	}
+	
+	/**
+	 * Metodo para agregar valores a la cola
+	 * @param valor
+	 */
+	public synchronized void put(int valor) {
+		
+		while(disponible == true) {
+			
+			try { wait(); }
+			catch (InterruptedException e) {}
+		}
+		
+		numero = valor;
+		disponible = true;
+		notifyAll();
+	}
+}
