45d834fa71d08e0b7d87299fad990d931d46dc5b
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-20 08:51:19 +0100

    Create Cola.java

---
+++ PSP/procesos/src/Hilos/Hilos11/Cola.java
@@ -0,0 +1,37 @@
+package Hilos.Hilos11;
+
+public class Cola {
+	
+	private int numero;
+	private boolean disponible = false;
+	
+	/**
+	 * Metodo para coger valores de la cola
+	 * @return
+	 */
+	public int get() {
+		
+		// Si disponible es true hay un numero en la cola para coger
+		if (disponible) {
+			
+			disponible = false;
+			return numero;
+		}
+		
+		// Devuelve -1 si no hay numero disponible
+		return -1;
+	}
+	
+	/**
+	 * Metodo para agregar valores a la cola
+	 * @param valor
+	 */
+	public void put(int valor) {
+		
+		// Coloca el valor en la cola
+		numero = valor;
+		
+		// Disponible para consumir, cola llena
+		disponible = true;
+	}
+}
