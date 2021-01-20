b44c7d352c186d19c7e8a5472b2789865eed1c20
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-20 12:01:40 +0100

    Update 26_Manejo_Datos

--- DI/26_Manejo_Datos/26_Manejo_Datos/Components/Json_Gest.xaml.cs
+++ DI/26_Manejo_Datos/26_Manejo_Datos/Components/Json_Gest.xaml.cs
@@ -1,9 +1,20 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
+using Newtonsoft.Json;
 
 namespace _26_Manejo_Datos.Components {
 
     public partial class Json_Gest : UserControl {
+
+        private string jsonPath = Environment.CurrentDirectory + "/data/sensores.json";
+        private string json = null;
+        private List<Sensor> mediciones = null;
+        private int indiceList = 0;
+        private Sensor currentReg = null, registro = null;
 
         /// <summary>Carga los componentes de la ventana</summary>
         public Json_Gest() => InitializeComponent();
@@ -13,7 +24,91 @@ namespace _26_Manejo_Datos.Components {
         /// <param name="e">Parametros de la accion</param>
         private void UserControl_Loaded(object sender, RoutedEventArgs e) {
 
-            // Cargar fichero JSON y mostrar primer registro
+            vaciarFormulario();
+
+            try {
+
+                json = File.ReadAllText(jsonPath);
+                mediciones = JsonConvert.DeserializeObject<List<Sensor>>(json);
+                registro = mediciones.ElementAt(indiceList);
+                visualizarRegistros(registro);
+            } 
+            catch (Exception err) { MessageBox.Show("Error al cargar el fichero. Pongase en contacto con el administrador."); }
+        }
+
+        /// <summary>Muestra el primer registro</summary>
+        /// <param name="sender">Elemento que realiza la accion</param>
+        /// <param name="e">Parametros de la accion</param>
+        private void btn_first_reg_Click(object sender, RoutedEventArgs e) {
+
+            indiceList = 0;
+            registro = mediciones.ElementAt(indiceList);
+            visualizarRegistros(registro);
+        }
+
+        /// <summary>Muestra el registro anterior</summary>
+        /// <param name="sender">Elemento que realiza la accion</param>
+        /// <param name="e">Parametros de la accion</param>
+        private void btn_previous_reg_Click(object sender, RoutedEventArgs e) {
+
+            indiceList = ((indiceList - 1) < 0) ? 0 : indiceList - 1;
+            registro = mediciones.ElementAt(indiceList);
+            visualizarRegistros(registro);
+        }
+
+        /// <summary>Muestra el siguiente registro</summary>
+        /// <param name="sender">Elemento que realiza la accion</param>
+        /// <param name="e">Parametros de la accion</param>
+        private void btn_next_reg_Click(object sender, RoutedEventArgs e) {
+
+            indiceList = ((indiceList + 1) > (mediciones.Count - 1)) ? (mediciones.Count - 1) : indiceList + 1;
+            registro = mediciones.ElementAt(indiceList);
+            visualizarRegistros(registro);
+        }
+
+        /// <summary>Muestra el ultimo registro</summary>
+        /// <param name="sender">Elemento que realiza la accion</param>
+        /// <param name="e">Parametros de la accion</param>
+        private void btn_last_reg_Click(object sender, RoutedEventArgs e) {
+
+            indiceList = mediciones.Count - 1;
+            registro = mediciones.ElementAt(indiceList);
+            visualizarRegistros(registro);
+        }
+
+        private void btn_delete_reg_Click(object sender, RoutedEventArgs e) {}
+
+        private void btn_save_reg_Click(object sender, RoutedEventArgs e) {}
+
+        private void btn_new_reg_Click(object sender, RoutedEventArgs e) {}
+
+        /// <summary>Limpia los campos del formulario</summary>
+        private void vaciarFormulario() {
+
+            tb_json_id.Text = "";
+            tb_json_descripcion.Text = "";
+            tb_json_fecha.Text = "";
+            tb_json_hora.Text = "";
+            tb_json_latitud.Text = "";
+            tb_json_longitud.Text = "";
+            tb_json_humedad.Text = "";
+            tb_json_temperatura.Text = "";
+        }
+
+        /// <summary>Muestra los registros de un sensor en los campos del formulario</summary>
+        /// <param name="r">Registros del sensor</param>
+        private void visualizarRegistros(Sensor r) {
+
+            currentReg = r;
+
+            tb_json_id.Text = r.Id.ToString();
+            tb_json_descripcion.Text = r.DescripcionSensor.ToString();
+            tb_json_fecha.Text = r.Fecha.ToString();
+            tb_json_hora.Text = r.Hora.ToString();
+            tb_json_latitud.Text = r.Latitud.ToString();
+            tb_json_longitud.Text = r.Longitud.ToString();
+            tb_json_humedad.Text = r.Humedad.ToString();
+            tb_json_temperatura.Text = r.Temperatura.ToString();
         }
     }
 }
---
+++ DI/26_Manejo_Datos/26_Manejo_Datos/Sensor.cs
@@ -0,0 +1,21 @@
+﻿namespace _26_Manejo_Datos {
+
+    class Sensor {
+
+        private int id;
+        private string descripcionSensor, fecha, hora, latitud, longitud;
+        private float humedad, temperatura;
+
+        public int Id { get => id; set => id = value; }
+        public string DescripcionSensor { get => descripcionSensor; set => descripcionSensor = value; }
+        public string Fecha { get => fecha; set => fecha = value; }
+        public string Hora { get => hora; set => hora = value; }
+        public string Latitud { get => latitud; set => latitud = value; }
+        public string Longitud { get => longitud; set => longitud = value; }
+        public float Humedad { get => humedad; set => humedad = value; }
+        public float Temperatura { get => temperatura; set => temperatura = value; }
+
+
+        /* ========== AQUI HAY QUE METER LAS VALIDACIONES DE ERRORES ========== */
+    }
+}
