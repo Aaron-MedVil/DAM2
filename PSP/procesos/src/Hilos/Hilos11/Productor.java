e1240839c216f5811a3cc0a598b58733a8188be2
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-20 08:51:35 +0100

    Create Productor.java

---
+++ PSP/procesos/src/Hilos/Hilos11/Productor.java
@@ -0,0 +1,32 @@
+package Hilos.Hilos11;
+
+public class Productor extends Thread {
+
+	private Cola c;
+	private int n;
+	
+	/**
+	 * Constructor de la clase
+	 * @param c
+	 * @param n
+	 */
+	public Productor(Cola c, int n) {
+		this.c = c;
+		this.n = n;
+	}
+	
+	/**
+	 * Metodo para ejecutar la clase
+	 */
+	public void run() {
+		
+		for (int i = 0; i < 5; i++) {
+			
+			c.put(i);
+			System.out.println(i + "=>Productor: " + n + " produce: " + i);
+			
+			try { sleep(100); }
+			catch (InterruptedException e) {}
+		}
+	}
+}
