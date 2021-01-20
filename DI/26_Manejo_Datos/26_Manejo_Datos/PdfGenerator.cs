434e5be072353bb80370d74ba66bdaeba2dec7ef
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-20 16:15:10 +0100

    Update 26_Manejo_Datos

--- DI/26_Manejo_Datos/26_Manejo_Datos/PdfGenerator.cs
+++ DI/26_Manejo_Datos/26_Manejo_Datos/PdfGenerator.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Windows;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 
@@ -13,98 +15,101 @@ namespace _26_Manejo_Datos {
         /// <summary>Genera el fichero PDF del JSON</summary>
         /// <param name="mediciones">Datos que guardaremos en el pdf</param>
         public void pdfJSON(List<Sensor> mediciones, string pdfName) {
-            
-            // Crea el fichero PDF y lo abre
-            FileStream fs = new FileStream(pdfName, FileMode.Create, FileAccess.Write, FileShare.None);
-            Document doc = new Document(PageSize);
-            PdfWriter writer = PdfWriter.GetInstance(doc, fs);
-            doc.Open();
-
-            /* ========== CABECERA ========== */
-            Chunk chkTitle = new Chunk("Listado Sensores", FontFactory.GetFont(FontFactory.HELVETICA, 24f, Font.BOLD, new BaseColor(0, 210, 120)));
-            Phrase textotituloListado = new Phrase(18f, chkTitle);
-            doc.Add(new Paragraph(textotituloListado));
 
             string[] nomCols = new string[] { "Id", "Sensor", "Fecha", "Hora", "Humedad", "Temperatura" };
-
-            /* ========== TABLA DE CABECERA ========== */
-            PdfPTable titulos = new PdfPTable(nomCols.Length) {
-                DefaultCell = {
-                    HorizontalAlignment = Element.ALIGN_CENTER,
-                    VerticalAlignment = Element.ALIGN_CENTER,
-                    BackgroundColor = new BaseColor(199, 226, 226)
-                }
-            };
-
             float[] anchoColumnas = new float[] { 50f, 250f, 100f, 100f, 75f, 75f };
+            float yPos = 920f;
 
-            // Asignar el array de anchos de columna a la tabla
-            titulos.SetTotalWidth(anchoColumnas);
+            try {
 
-            titulos.AddCell("Id");
-            titulos.AddCell("Sensor");
-            titulos.AddCell("Fecha");
-            titulos.AddCell("Hora");
-            titulos.AddCell("Humedad");
-            titulos.AddCell("Temperatura");
+                // Crea el fichero PDF y lo abre
+                FileStream fs = new FileStream(pdfName, FileMode.Create, FileAccess.Write, FileShare.None);
+                Document doc = new Document(PageSize);
+                PdfWriter writer = PdfWriter.GetInstance(doc, fs);
+                doc.Open();
 
-            titulos.WriteSelectedRows(0, -1, 10f, 940f, writer.DirectContent);
+                /* ========== CABECERA ========== */
+                BaseColor foregroundTitle = new BaseColor(0, 210, 120);
+                Font fontTitle = FontFactory.GetFont(FontFactory.HELVETICA, 24f, Font.BOLD, foregroundTitle);
+                Chunk chkTitle = new Chunk("Listado Sensores", fontTitle);
+                Phrase textTitle = new Phrase(0f, chkTitle);
+                doc.Add(new Paragraph(textTitle));
 
-            /* ========== TABLA DE DATOS ========== */
-            PdfPTable datos = new PdfPTable(nomCols.Length) {
-                DefaultCell = {
-                    HorizontalAlignment = Element.ALIGN_CENTER,
-                    VerticalAlignment = Element.ALIGN_CENTER,
-                }
-            };
+                /* ========== TABLA DE CABECERA ========== */
+                PdfPTable titulos = new PdfPTable(nomCols.Length) {
+                    DefaultCell = {
+                        HorizontalAlignment = Element.ALIGN_CENTER,
+                        VerticalAlignment = Element.ALIGN_CENTER,
+                        BackgroundColor = new BaseColor(199, 226, 226)
+                    }
+                };
 
-            // Asignar el array de anchos de columna a la tabla
-            datos.SetTotalWidth(anchoColumnas);
+                // Asignar el array de anchos de columna a la tabla
+                titulos.SetTotalWidth(anchoColumnas);
+                foreach (string col in nomCols) { titulos.AddCell(col); }
+                titulos.WriteSelectedRows(0, -1, 10f, 940f, writer.DirectContent);
 
-            int cf = 0;
+                /* ========== TABLA DE DATOS ========== */
+                PdfPTable datos = new PdfPTable(nomCols.Length) {
+                    DefaultCell = {
+                        HorizontalAlignment = Element.ALIGN_CENTER,
+                        VerticalAlignment = Element.ALIGN_CENTER,
+                    }
+                };
+                datos.SetTotalWidth(anchoColumnas);
 
-            foreach (Sensor r in mediciones) {
+                // Rellenamos la tabla de datos
+                foreach (Sensor r in mediciones) {
 
-                datos.AddCell(r.Id.ToString());
-                datos.AddCell(r.DescripcionSensor.ToString());
-                datos.AddCell(r.Fecha.ToString());
-                datos.AddCell(r.Hora.ToString());
-                datos.AddCell(r.Temperatura.ToString());
-                datos.AddCell(r.Humedad.ToString());
+                    datos.AddCell(r.Id.ToString());
+                    datos.AddCell(r.DescripcionSensor.ToString());
+                    datos.AddCell(r.Fecha.ToString());
+                    datos.AddCell(r.Hora.ToString());
+                    datos.AddCell(r.Temperatura.ToString());
+                    datos.AddCell(r.Humedad.ToString());
 
-                cf++;
+                    // Asigna la posicion Y y resta el ancho de la columna para el siguiente registro
+                    datos.WriteSelectedRows(0, -1, 10f, yPos, writer.DirectContent);
 
-                if (cf > 1) {
-
-                    //Salto de página
-                    datos.WriteSelectedRows(0, -1, 10f, 920f, writer.DirectContent);
-
-                    // Borrar los datos de la PdfTable
-                    datos = new PdfPTable(nomCols.Length);
+                    // Limpiamos los datos de la columna
+                    datos = new PdfPTable(nomCols.Length) {
+                        DefaultCell = {
+                            HorizontalAlignment = Element.ALIGN_CENTER,
+                            VerticalAlignment = Element.ALIGN_CENTER,
+                        }
+                    };
                     datos.SetTotalWidth(anchoColumnas);
 
-                    // Página nueva
-                    doc.NewPage();
+                    if (yPos > 60) { yPos = yPos - 15; }
+                    else {
 
-                    doc.Add(new Paragraph(textotituloListado));
+                        yPos = 1000f - 20;
 
-                    titulos.WriteSelectedRows(0, -1, 10f, 940f, writer.DirectContent);
+                        // Página nueva
+                        doc.NewPage();
+                        titulos.WriteSelectedRows(0, -1, 10f, 1000f, writer.DirectContent);
+                    }
                 }
+
+                // Cierra el fichero
+                doc.Close();
+
+                // Muestra el fichero
+                openPdf(pdfName);
             }
-
-            datos.WriteSelectedRows(0, -1, 10f, 970f, writer.DirectContent);
-
-            doc.Close();
-
-            openPdf(pdfName);
+            catch (Exception err) { MessageBox.Show("Se ha producido un error al generar el fichero PDF"); }
         }
 
         /// <summary>Abre un fichero PDF</summary>
         /// <param name="pdfName">Ruta del fichero PDF</param>
         public void openPdf(string pdfName) {
-            Process p = new Process();
-            p.StartInfo = new ProcessStartInfo(@pdfName) { UseShellExecute = true };
-            p.Start();
+
+            try {
+                Process p = new Process();
+                p.StartInfo = new ProcessStartInfo(@pdfName) { UseShellExecute = true };
+                p.Start();
+            }
+            catch (Exception err) { MessageBox.Show("Error al abrir el fichero " + pdfName.ToString()); }
         }
     }
 }
