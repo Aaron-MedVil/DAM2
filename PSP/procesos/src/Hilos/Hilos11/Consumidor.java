4179302d5a0437f2bdb64c358933f1447fd0810e
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-20 08:51:24 +0100

    Create Consumidor.java

---
+++ PSP/procesos/src/Hilos/Hilos11/Consumidor.java
@@ -0,0 +1,31 @@
+package Hilos.Hilos11;
+
+public class Consumidor extends Thread {
+	
+	private Cola c;
+	private int n;
+	
+	/**
+	 * Constructor de la clase
+	 * @param c
+	 * @param n
+	 */
+	public Consumidor(Cola c, int n) {
+		this.c = c;
+		this.n = n;
+	}
+	
+	/**
+	 * Metodo para ejecutar la clase
+	 */
+	public void run() {
+		
+		int valor = 0;
+		
+		for (int i = 0; i < 5; i++) {
+			
+			valor = c.get();
+			System.out.println(i + "=>Consumidor: " + n + " consume: " + valor);
+		}
+	}
+}
