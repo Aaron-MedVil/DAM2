b0b12b79f65e64643013755ab86d91dbf64fcabc
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-21 12:35:00 +0100

    Update 26_Manejo_Datos

--- DI/26_Manejo_Datos/26_Manejo_Datos/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ DI/26_Manejo_Datos/26_Manejo_Datos/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -103,7 +103,7 @@ namespace _26_Manejo_Datos {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/26_Manejo_Datos;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/26_Manejo_Datos;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
