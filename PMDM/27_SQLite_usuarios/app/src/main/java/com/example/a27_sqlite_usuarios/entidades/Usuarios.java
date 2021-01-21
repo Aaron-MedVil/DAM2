f2d9b076bf44617747504121758a98082cb776a6
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-21 13:18:03 +0100

    Create Usuarios.java

---
+++ PMDM/27_SQLite_usuarios/app/src/main/java/com/example/a27_sqlite_usuarios/entidades/Usuarios.java
@@ -0,0 +1,15 @@
+package com.example.a27_sqlite_usuarios.entidades;
+
+public class Usuarios {
+
+    private String dni, nombre, telefono;
+
+    public String getDni() { return dni; }
+    public void setDni(String dni) { this.dni = dni; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+}
