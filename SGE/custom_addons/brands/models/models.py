07ec8d886196bfa4a87adc0b2460dc55e163dc3d
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-21 09:55:58 +0100

    Update custom_addons

--- SGE/custom_addons/brands/models/models.py
+++ SGE/custom_addons/brands/models/models.py
@@ -2,16 +2,17 @@
 
 from odoo import models, fields, api
 
-
+## Modelo del formulario para crear un registro
 class Brand(models.Model):
     _name = 'brand.template'
     _description = 'Brands'
 
     name = fields.Char(string="Brand")
     logo = fields.Image(string="Logo")
+    description = fields.Char(string="Description")
 
+## Modelo para la vista de producto
 class CustomBrand(models.Model):
     _inherit = 'product.template'
     
     brand = fields.Selection([("N", "Nike"), ("A", "Adidas")], string="Brand", help="Seleccione la marca")
-
