d5c7e3c1e8e33d575ec4cecd85425e5d75fc06e0
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-21 13:39:56 +0100

    Update Main.java

--- PSP/procesos/src/Red/Red01/Main.java
+++ PSP/procesos/src/Red/Red01/Main.java
@@ -10,12 +10,26 @@ public class Main {
 		String pcName = "DESKTOP-RBFFHNS";
 		InetAddress dir = null;
 		
-		System.out.println("========================================");
-		System.out.println("SALIDA PARA LOCALHOST");
-		
 		try {
+			
+			// Nombre del equipo
+			System.out.println("========================================");
+			System.out.println("SALIDA PARA LOCALHOST");
 			dir = InetAddress.getByName(pcName);
 			probarMetodos(dir);
+			
+			// URL www.google.es
+			System.out.println("========================================");
+			System.out.println("SALIDA PARA UNA URL");
+			dir = InetAddress.getByName("www.google.es");
+			probarMetodos(dir);
+			
+			// Direcciones IP de una UR Lwww.google.es
+			System.out.println("\t DIRECCIONES IP PARA: " + dir.getHostName());
+			InetAddress[] direcciones = InetAddress.getAllByName(dir.getHostName());
+			for (int i = 0; i < direcciones.length; i++) {
+				System.out.println("\t\t" + direcciones[i].toString());
+			}
 		}
 		catch (UnknownHostException e) { e.printStackTrace(); }
 	}
@@ -30,6 +44,7 @@ public class Main {
 			dir2 = InetAddress.getLocalHost();
 			System.out.println("\t Método getLocalHost(): " + dir2);
 			System.out.println("\t Método getHostAddress(): " + dir.getHostAddress());
+			System.out.println("\t Metodo getCanonicalHostName(): " + dir.getCanonicalHostName());
 		}
 		catch (UnknownHostException e) { e.printStackTrace(); }
 	}
