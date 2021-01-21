a949c79e4414750c5d418d0db70b838f13c88623
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-21 13:18:11 +0100

    Update MainActivity.java

--- PMDM/27_SQLite_usuarios/app/src/main/java/com/example/a27_sqlite_usuarios/MainActivity.java
+++ PMDM/27_SQLite_usuarios/app/src/main/java/com/example/a27_sqlite_usuarios/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.a27_sqlite_usuarios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Metodo que se ejecuta cuando se crea la clase
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DbConn conn = new DbConn(this, "dbUsuarios", null, 1);
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void registrarUsuario(View view) {
+
+        // Intent intent = new Intent(MainActivity.this, );
     }
 }
