104ea1a8f8a678b4924be7c006a522df57ba7347
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-22 10:45:04 +0100

    Update Main.java

--- AD/UT2/src/db4o/Main.java
+++ AD/UT2/src/db4o/Main.java
@@ -1,6 +1,32 @@
 package db4o;
 
-public class Main {
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
 
-	public static void main(String[] args) {}
+public class Main {
+	
+	// Nombre de la base de datos
+	private static String BDPer = "data/DBPersonas.yap";
+
+	/**
+	 * Metodo principal del programa
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Creamos y abrimos la base de datos
+		ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), BDPer);
+		
+		// Creamos registro para la base de datos
+		Persona p1 = new Persona("Juan", "Guadalajara");
+		Persona p2 = new Persona("Ana", "Madrid");
+		Persona p3 = new Persona("Luis", "Granada");
+		Persona p4 = new Persona("Pedro", "Asturias");
+		
+		// Almacenamos los registros en la base de datos
+		db.store(p1); db.store(p2); db.store(p3); db.store(p4);
+		
+		// Cerramos la base de datos
+		db.close();
+	}
 }
