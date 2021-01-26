348a2610f1704ee614a4c666da350c8dc6715ffc
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-26 08:47:57 +0100

    Update ConsultarUsuario.java

--- PMDM/27_SQLite_usuarios/app/src/main/java/com/example/a27_sqlite_usuarios/ConsultarUsuario.java
+++ PMDM/27_SQLite_usuarios/app/src/main/java/com/example/a27_sqlite_usuarios/ConsultarUsuario.java
@@ -7,21 +7,41 @@ import android.view.View;
 
 public class ConsultarUsuario extends AppCompatActivity {
 
+    /**
+     * Metodo que se ejecuta cuando se crea la clase
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultar_usuario);
     }
 
-    public void click_btn_limpiar_consultar_usuario(View view) {
-    }
-
-    public void click_btn_eliminar_consultar_usuario(View view) {
-    }
-
+    /**
+     * Busca los datos de un usuario
+     * @param view
+     */
     public void click_btn_buscar_consultar_usuario(View view) {
     }
 
+    /**
+     * Boton para limpiar los campos del formulario
+     * @param view
+     */
+    public void click_btn_limpiar_consultar_usuario(View view) {
+    }
+
+    /**
+     * Elimina los datos de la base de datos del usuario buscado
+     * @param view
+     */
+    public void click_btn_eliminar_consultar_usuario(View view) {
+    }
+
+    /**
+     * Actualiza los datos de la base de datos del usuario buscado
+     * @param view
+     */
     public void click_btn_actualizar_consultar_usuario(View view) {
     }
 }
