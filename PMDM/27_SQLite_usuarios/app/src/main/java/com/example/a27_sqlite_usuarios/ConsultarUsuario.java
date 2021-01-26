3e337a20331327f839e70a4c04e9a357afb04f24
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-26 08:39:36 +0100

    Update ConsultarUsuario.java

--- PMDM/27_SQLite_usuarios/app/src/main/java/com/example/a27_sqlite_usuarios/ConsultarUsuario.java
+++ PMDM/27_SQLite_usuarios/app/src/main/java/com/example/a27_sqlite_usuarios/ConsultarUsuario.java
@@ -3,6 +3,7 @@ package com.example.a27_sqlite_usuarios;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ConsultarUsuario extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class ConsultarUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultar_usuario);
+    }
+
+    public void click_btn_limpiar_consultar_usuario(View view) {
+    }
+
+    public void click_btn_eliminar_consultar_usuario(View view) {
+    }
+
+    public void click_btn_buscar_consultar_usuario(View view) {
+    }
+
+    public void click_btn_actualizar_consultar_usuario(View view) {
     }
 }
