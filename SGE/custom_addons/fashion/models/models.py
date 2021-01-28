b2f2da2b6edbb6f52f5ec65927880fb850548838
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-28 12:35:55 +0100

    Update fashion module

--- SGE/custom_addons/fashion/models/models.py
+++ SGE/custom_addons/fashion/models/models.py
@@ -2,9 +2,18 @@
 
 from odoo import models, fields, api
 
+## Modelo que crea los campos que utilizaremos para añadir informacion
 class fashion(models.Model):
     _name = 'fashion.fashion'
     _description = 'fashion.fashion'
     
     # Campos
-    name = fields.Char()
+    # name = fields.Char(string="Name")
+
+class seasson(models.Model):
+    _name = 'seassons.template'
+    _description = 'seassons.template'
+
+    name = fields.Char(string="Name", required=True)
+    startDate = fields.Date('Start date', required=True, default=fields.Date.today())
+    endDate = fields.Date('End date')
