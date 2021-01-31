f0934b27acab0347e75f39ea54f713e3d3e13c9d
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-31 12:49:50 +0100

    Create Departamentos.java

---
+++ AD/UT2/src/db4o_trabajo/Ej1/Departamentos.java
@@ -0,0 +1,49 @@
+package db4o_trabajo.Ej1;
+
+public class Departamentos {
+
+	private String dnombre, loc;
+	private int dept_no;
+	
+	/**
+	 * Constructor con parametros
+	 * @param dnombre
+	 * @param loc
+	 * @param dept_no
+	 */
+	public Departamentos(String dnombre, String loc, int dept_no) {
+		this.dnombre = dnombre;
+		this.loc = loc;
+		this.dept_no = dept_no;
+	}
+	
+	/**
+	 * Constructor sin parametros
+	 */
+	public Departamentos() {
+		this.dnombre = null;
+		this.loc = null;
+		this.dept_no = 0;
+	}
+	
+	/**
+	 * Getter/Setter nombre
+	 * @return
+	 */
+	public String getDnombre() { return dnombre; }
+	public void setDnombre(String dnombre) { this.dnombre = dnombre; }
+	
+	/**
+	 * Getter/Setter localizacion
+	 * @return
+	 */
+	public String getLoc() { return loc; }
+	public void setLoc(String loc) { this.loc = loc; }
+	
+	/**
+	 * Getter/Setter nombre departamento
+	 * @return
+	 */
+	public int getDept_no() { return dept_no; }
+	public void setDept_no(int dept_no) { this.dept_no = dept_no; }
+}
