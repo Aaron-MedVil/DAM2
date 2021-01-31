e6de54feb42d7aa4768df184223d4bae31d7bbbd
xaxe96 <a.medina.v.96@hotmail.com>
2021-01-31 13:09:24 +0100

    Update Persona.java

--- AD/UT2/src/DB4o_ejemplo/Persona.java
+++ AD/UT2/src/DB4o_ejemplo/Persona.java
@@ -1,35 +1,38 @@
 package DB4o_ejemplo;
 
 public class Persona {
-	private String nombre;
-	private String ciudad;
+	
+	private String nombre, ciudad;
 
+	/**
+	 * Constructor con parametros
+	 * @param nombre
+	 * @param ciudad
+	 */
 	public Persona(String nombre, String ciudad) {
 		this.nombre = nombre;
 		this.ciudad = ciudad;
 	}
 
+	/**
+	 * Constructor vacio
+	 */
 	public Persona() {
 		this.nombre = null;
 		this.ciudad = null;
 	}
 
-	
+	/**
+	 * Getter/Setter nombre
+	 * @return
+	 */
+	public String getNombre() { return nombre; }
+	public void setNombre(String nombre) { this.nombre = nombre; }
 
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getCiudad() {
-		return ciudad;
-	}
-
-	public void setCiudad(String ciudad) {
-		this.ciudad = ciudad;
-	}
-
-}// fin Persona
+	/**
+	 * Getter/Setter ciudad
+	 * @return
+	 */
+	public String getCiudad() { return ciudad; }
+	public void setCiudad(String ciudad) { this.ciudad = ciudad; }
+}
