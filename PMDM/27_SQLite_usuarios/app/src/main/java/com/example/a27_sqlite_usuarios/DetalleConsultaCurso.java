0d02a409984cc3ccd8ce39c4c72978d4ccb0a54f
xaxe96 <a.medina.v.96@hotmail.com>
2021-02-02 10:22:50 +0100

    Update DetalleConsultaCurso.java

--- PMDM/27_SQLite_usuarios/app/src/main/java/com/example/a27_sqlite_usuarios/DetalleConsultaCurso.java
+++ PMDM/27_SQLite_usuarios/app/src/main/java/com/example/a27_sqlite_usuarios/DetalleConsultaCurso.java
@@ -3,6 +3,7 @@ package com.example.a27_sqlite_usuarios;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.a27_sqlite_usuarios.entidades.Cursos;
@@ -10,14 +11,31 @@ import com.example.a27_sqlite_usuarios.entidades.Usuarios;
 
 public class DetalleConsultaCurso extends AppCompatActivity {
 
+    EditText et_id_curso, et_nombre_curso, et_duracion_curso, et_dni_usuario, et_nombre_usuario, et_telefono_usuario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_consulta_curso);
 
-        Usuarios usuario = (Usuarios)getIntent().getSerializableExtra("usuario");
-        Cursos c = (Cursos)getIntent().getSerializableExtra("cursos");
+        Usuarios usuario = (Usuarios) getIntent().getSerializableExtra("usuario");
+        Cursos curso = (Cursos) getIntent().getSerializableExtra("cursos");
 
-        Toast.makeText(getApplicationContext(), usuario.getNombre(), Toast.LENGTH_SHORT).show();
+        et_id_curso = findViewById(R.id.et_id_curso);
+        et_nombre_curso = findViewById(R.id.et_nombre_curso);
+        et_duracion_curso = findViewById(R.id.et_duracion_curso);
+        et_dni_usuario = findViewById(R.id.et_dni_usuario);
+        et_nombre_usuario = findViewById(R.id.et_nombre_usuario);
+        et_telefono_usuario = findViewById(R.id.et_telefono_usuario);
+
+        // Datos del curso
+        et_id_curso.setText(curso.getId_curso());
+        et_nombre_curso.setText(curso.getNombre_curso());
+        et_duracion_curso.setText(curso.getDuracion());
+
+        // Datos del usuario
+        et_dni_usuario.setText(usuario.getDni());
+        et_nombre_usuario.setText(usuario.getNombre());
+        et_telefono_usuario.setText(usuario.getTelefono());
     }
 }
