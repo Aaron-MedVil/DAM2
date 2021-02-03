11a3cd01410969eae65f8e531dacbf21c67c1622
xaxe96 <a.medina.v.96@hotmail.com>
2021-02-03 09:47:43 +0100

    Update Main.java

--- AD/UT2/src/Mysql/Mysql_trabajo/Main.java
+++ AD/UT2/src/Mysql/Mysql_trabajo/Main.java
@@ -3,32 +3,104 @@ package Mysql.Mysql_trabajo;
 import java.sql.*;
 
 public class Main {
+	
+	private static String strConn = "jdbc:mysql://localhost:3306/neptuno", dbUser = "neptuno", dbPass = "neptuno";
 
+	/**
+	 * Metodo que se ejecuta al iniciar la clase
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
+		/* ========== AVISO ========== */
+		// El conector que nos mandastes me daba fallo en mi base de datos, por lo que
+		// buscando por internet recomendaban usar un conector más moderno.
+		// En el proyecto te envío también el conector que he utilizado.
+
+		// Por otro lado al ejecutar la sentencia "Class.forName("com.mysql.jdbc.Driver");"
+		// me saltaba un error, y al quitarla funciona perfectamente.
+
+		getDataClientes();
+		// getDataPedidos();
+	}
+
+	/**
+	 * Obtiene los datos de clientes
+	 */
+	private static void getDataClientes() {
+
 		try {
-			
-			// Cargar el driver
-			Class.forName("com.mysql.jdbc.Driver");
-			
-			// Establecemos la conexión con la BD - getConnection(String URL, String user, String pass)
-			Connection conexión = DriverManager.getConnection("jdbc:mysql://localhost:3306/neptuno", "neptuno", "neptuno");
-			
+
+			// Establecemos la conexión con la BD
+			Connection conexión = DriverManager.getConnection(strConn, dbUser, dbPass);
+
 			// Preparamos la consulta
 			Statement sentencia = conexión.createStatement();
-			String sql = "SELECT `Id_Cliente`, `Nombre_Compania`, `Nombre_Contacto`, `Ciudad` FROM `clientes` WHERE 1";
-			ResultSet resul = sentencia.executeQuery(sql);
 			
-			// Recorremos el resultado para visualizar cada fila,se hace un bucle mientras
-			// haya registros y se van mostrando
-			while (resul.next()) {
-				System.out.printf("%d %n", resul.getInt(1));
+			// Cadena de la consulta
+			String sql = "SELECT `Id_Cliente`, `Nombre_Compania`, `Nombre_Contacto`, `Ciudad` FROM `clientes`";
+			
+			// Ejecutamos la sentencia
+			ResultSet result = sentencia.executeQuery(sql);
+
+			// Mostramos los registros de la base de datos
+			while (result.next()) {
+				System.out.println("DATOS DEL USUARIO ID " + result.getInt(1));
+				System.out.println("\tNombre de la compañía: " + result.getString(2));
+				System.out.println("\tNombre del contacto: " + result.getString(3));
+				System.out.println("\tCiudad: " + result.getString(4));
+				System.out.println("============================================================");
 			}
-			resul.close(); // Cerrar
-			sentencia.close(); // Cerrar Statement
-			conexión.close(); // Cerrar conexión
+			
+			// Cerramos el ResultSet
+			result.close();
+			
+			// Cerramos la sentencia
+			sentencia.close();
+			
+			// Cerramos la conexion
+			conexión.close();
 		}
-		catch (ClassNotFoundException cn) { cn.printStackTrace(); }
+		catch (SQLException e) { e.printStackTrace(); }
+	}
+	
+	/**
+	 * Obtiene los datos de pedidos
+	 */
+	private static void getDataPedidos() {
+		
+		try {
+
+			// Establecemos la conexión con la BD
+			Connection conexión = DriverManager.getConnection(strConn, dbUser, dbPass);
+
+			// Preparamos la consulta
+			Statement sentencia = conexión.createStatement();
+			
+			// Cadena de la consulta
+			String sql = "SELECT `Id_Pedidos`, `Id_Empleado`, DATE_FORMAT(`Fecha_Pedido`, '%d-%m-%Y'), `Id_Cliente` FROM `pedidos`";
+			
+			// Ejecutamos la sentencia
+			ResultSet result = sentencia.executeQuery(sql);
+
+			// Mostramos los registros de la base de datos
+			while (result.next()) {
+				System.out.println("DATOS DEL PEDIDO ID " + result.getInt(1));
+				System.out.println("\tID Empleado: " + result.getInt(2));
+				System.out.println("\tFecha del pedido: " + result.getString(3));
+				System.out.println("\tID Cliente: " + result.getInt(4));
+				System.out.println("============================================================");
+			}
+			
+			// Cerramos el ResultSet
+			result.close();
+			
+			// Cerramos la sentencia
+			sentencia.close();
+			
+			// Cerramos la conexion
+			conexión.close();
+		}
 		catch (SQLException e) { e.printStackTrace(); }
 	}
 }
