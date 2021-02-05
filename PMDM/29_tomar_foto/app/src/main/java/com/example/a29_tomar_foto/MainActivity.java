26fbfbd42ae1aa3c023450d0f7de4d816efab09d
xaxe96 <a.medina.v.96@hotmail.com>
2021-02-05 09:20:11 +0100

    Update 29_tomar_foto

--- PMDM/29_tomar_foto/app/src/main/java/com/example/a29_tomar_foto/MainActivity.java
+++ PMDM/29_tomar_foto/app/src/main/java/com/example/a29_tomar_foto/MainActivity.java
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView imgCamera;
+    private String currentPhotoPath;
 
     /**
      * Metodo que se ejecuta cuando se crea la actividad
@@ -56,5 +63,22 @@ public class MainActivity extends AppCompatActivity {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             imgCamera.setImageBitmap(imageBitmap);
         }
+    }
+
+    private File createImageFile() throws IOException {
+
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
