3a92bdeb660e60ab77c3584e0d4ef6a503e0c552
xaxe96 <a.medina.v.96@hotmail.com>
2021-02-05 08:53:58 +0100

    Update 29_tomar_foto

--- PMDM/29_tomar_foto/app/src/main/java/com/example/a29_tomar_foto/MainActivity.java
+++ PMDM/29_tomar_foto/app/src/main/java/com/example/a29_tomar_foto/MainActivity.java
@@ -2,17 +2,59 @@ package com.example.a29_tomar_foto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView imgCamera;
+
+    /**
+     * Metodo que se ejecuta cuando se crea la actividad
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgCamera = findViewById(R.id.imgCamera);
     }
 
+    /**
+     * Metodo que se ejecuta cuando se pulsa la imagen y abre la camara
+     * @param view
+     */
     public void inClickCamera(View view) {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    /**
+     * Metodo que se ejecuta cuando se toma la foto
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgCamera.setImageBitmap(imageBitmap);
+        }
     }
 }
