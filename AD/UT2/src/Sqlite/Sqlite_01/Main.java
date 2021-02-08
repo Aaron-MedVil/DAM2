db298a6de324a92f44e0db6780173047b3aabc60
xaxe96 <a.medina.v.96@hotmail.com>
2021-02-08 09:33:39 +0100

    Update Main.java

--- AD/UT2/src/Sqlite/Sqlite_01/Main.java
+++ AD/UT2/src/Sqlite/Sqlite_01/Main.java
@@ -8,32 +8,29 @@ import java.sql.Statement;
 
 public class Main {
 
-private static String strConn = "jdbc:odbc:Mysql-odbc:mysql://localhost:3306/neptuno", dbUser = "neptuno", dbPass = "neptuno";
+	private static String strConn = "jdbc:sqlite:D:/Github/DAM2/AD/UT2/data/Ejemplo.db";
 	
 	public static void main(String[] args) {
 		
 		try {
-			
-			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
 
 			// Establecemos la conexión con la BD
-			Connection conexión = DriverManager.getConnection(strConn, dbUser, dbPass);
+			Connection conexión = DriverManager.getConnection(strConn);
 
 			// Preparamos la consulta
 			Statement sentencia = conexión.createStatement();
 			
 			// Cadena de la consulta
-			String sql = "SELECT `Id_Cliente`, `Nombre_Compania`, `Nombre_Contacto`, `Ciudad` FROM `clientes`";
+			String sql = "SELECT * FROM departamentos";
 			
 			// Ejecutamos la sentencia
 			ResultSet result = sentencia.executeQuery(sql);
 
 			// Mostramos los registros de la base de datos
 			while (result.next()) {
-				System.out.println("DATOS DEL USUARIO ID " + result.getInt(1));
-				System.out.println("\tNombre de la compañía: " + result.getString(2));
-				System.out.println("\tNombre del contacto: " + result.getString(3));
-				System.out.println("\tCiudad: " + result.getString(4));
+				System.out.println("DATOS DEL DEPARTAMENTO ID " + result.getInt(1));
+				System.out.println("\tNombre: " + result.getString(2));
+				System.out.println("\tUbicación: " + result.getString(3));
 				System.out.println("============================================================");
 			}
 			
@@ -46,7 +43,6 @@ private static String strConn = "jdbc:odbc:Mysql-odbc:mysql://localhost:3306/nep
 			// Cerramos la conexion
 			conexión.close();
 		}
-		catch (ClassNotFoundException cn) { cn.printStackTrace(); }
 		catch (SQLException e) { e.printStackTrace(); }
 	}
 }
