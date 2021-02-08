db314cf4f44fda8465cc8abd12cb48b845f1cdf9
xaxe96 <a.medina.v.96@hotmail.com>
2021-02-08 09:58:31 +0100

    Create Main.java

---
+++ AD/UT2/src/ApacheDerby/ApacheDerby_01/Main.java
@@ -0,0 +1,49 @@
+package ApacheDerby.ApacheDerby_01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+	
+	private static String s = "D:/Programs/Derby/Dbs/";
+	private static String strConn = "jdbc:derby:" + s + "ejemplo/";
+	
+	public static void main(String[] args) {
+		
+		try {
+
+			// Establecemos la conexión con la BD
+			Connection conexión = DriverManager.getConnection(strConn);
+
+			// Preparamos la consulta
+			Statement sentencia = conexión.createStatement();
+			
+			// Cadena de la consulta
+			String sql = "SELECT * FROM departamentos";
+			
+			// Ejecutamos la sentencia
+			ResultSet result = sentencia.executeQuery(sql);
+
+			// Mostramos los registros de la base de datos
+			while (result.next()) {
+				System.out.println("DATOS DEL DEPARTAMENTO ID " + result.getInt(1));
+				System.out.println("\tNombre: " + result.getString(2));
+				System.out.println("\tUbicación: " + result.getString(3));
+				System.out.println("============================================================");
+			}
+			
+			// Cerramos el ResultSet
+			result.close();
+			
+			// Cerramos la sentencia
+			sentencia.close();
+			
+			// Cerramos la conexion
+			conexión.close();
+		}
+		catch (SQLException e) { e.printStackTrace(); }
+	}
+}
