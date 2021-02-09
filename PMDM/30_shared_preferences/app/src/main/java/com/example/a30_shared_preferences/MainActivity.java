108843c05ff0c609c53b6c7b39c327069cc8501a
xaxe96 <a.medina.v.96@hotmail.com>
2021-02-09 09:53:18 +0100

    Create 30_shared_preferences

---
+++ PMDM/30_shared_preferences/app/src/main/java/com/example/a30_shared_preferences/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.a30_shared_preferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etuser, etpassword;
+    private Button btn_save, btn_entrar;
+
+    /**
+     * Metodo que se ejecuta al ejecutar la clase
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etuser = findViewById(R.id.etuser);
+        etpassword = findViewById(R.id.etpassword);
+
+        cargarCredenciaes();
+    }
+
+    /**
+     * Carga las credenciales del fichero credenciales
+     */
+    private void cargarCredenciaes() {
+
+        // Abrimos el fichero credenciales con permisos privados
+        SharedPreferences preferencias = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+
+        // Obtenemos las variables de preferencias
+        String user = preferencias.getString("user", null);
+        String pass = preferencias.getString("pass", null);
+
+        // Insertamos en los EditText los valores de las credenciales
+        etuser.setText(user);
+        etpassword.setText(pass);
+    }
+
+    /**
+     * Guarda las credenciales en el fichero credenciales
+     * @param view
+     */
+    public void guardarCredenciales(View view) {
+
+        // Abrimos el fichero credenciales con permisos privados
+        SharedPreferences preferencias = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+
+        // Obtenemos los datos de los EditText
+        String user = etuser.getText().toString();
+        String pass = etpassword.getText().toString();
+
+        // Guardamos mediante un objeto editor las variables de preferencias
+        SharedPreferences.Editor editor = preferencias.edit();
+        editor.putString("user", user);
+        editor.putString("pass", pass);
+        editor.commit();
+
+        Toast.makeText(this, "Credenciales guardadas", Toast.LENGTH_SHORT).show();
+    }
+}
