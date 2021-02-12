81959b71bfafe34efa0981f16cec249bacf564c8
xaxe96 <a.medina.v.96@hotmail.com>
2021-02-12 10:49:58 +0100

    Update Main.java

--- AD/UT2/src/HSQL/Main.java
+++ AD/UT2/src/HSQL/Main.java
@@ -8,32 +8,29 @@ import java.sql.Statement;
 
 public class Main {
 
-	private static String strConn = "jdbc:h2:D:/Programs/H2/DB/Clases/Clases";
+	private static String strConn = "jdbc:hsqldb:file:D:\\Programs\\hsqldb\\data\\Ejemplo";
 	
 	public static void main(String[] args) {
 		
 		try {
 
 			// Establecemos la conexión con la BD
-			Connection conexión = DriverManager.getConnection(strConn, "sa", "");
+			Connection conexión = DriverManager.getConnection(strConn);
 
 			// Preparamos la consulta
 			Statement sentencia = conexión.createStatement();
 			
 			// Cadena de la consulta
-			String sql = "SELECT * FROM cursos";
+			String sql = "SELECT * FROM departamentos";
 			
 			// Ejecutamos la sentencia
 			ResultSet result = sentencia.executeQuery(sql);
 
 			// Mostramos los registros de la base de datos
 			while (result.next()) {
-				System.out.println("DATOS DEL CURSO <<" + result.getString(1) + ">>");
+				System.out.println("DATOS DEL DEPARTAMENTO ID " + result.getInt(1));
 				System.out.println("\tNombre: " + result.getString(2));
-				System.out.println("\tFecha inicio: " + result.getString(3));
-				System.out.println("\tDuracion: " + result.getInt(4));
-				System.out.println("\tProfesor: " + result.getString(5));
-				System.out.println("\tPrecio: " + result.getInt(6));
+				System.out.println("\tUbicación: " + result.getString(3));
 				System.out.println("============================================================");
 			}
 			
