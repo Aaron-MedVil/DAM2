3f051535dac1db39404b95595e1570340cfc60cf
xaxe96 <a.medina.v.96@hotmail.com>
2021-02-17 10:07:52 +0100

    Create Main.java

---
+++ PSP/procesos/src/Red/Ejercicio_03/Main.java
@@ -0,0 +1,15 @@
+// Sockets UDP: Crea un programa donde el cliente envíe un texto tecleado al servidor,
+// el servidor lee el datagrama del servidor y le devuelve el mismo mensaje añadiendo RECIBIDO POR EL SERVIDOR.
+// El programa cliente recibe un datagrama del servidor y muestra información del mismo por pantalla (IP, puerto del servidor).
+// El programa servidor finaliza cuando recibe como cadena un asterisco.
+
+package Red.Ejercicio_03;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
