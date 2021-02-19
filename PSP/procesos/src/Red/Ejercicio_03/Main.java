97c5585dd153d799a1505b2ee2390241031ac5a5
xaxe96 <a.medina.v.96@hotmail.com>
2021-02-19 12:23:10 +0100

    Update Main.java

--- PSP/procesos/src/Red/Ejercicio_03/Main.java
+++ PSP/procesos/src/Red/Ejercicio_03/Main.java
@@ -8,29 +8,61 @@ package Red.Ejercicio_03;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		byte[] buffer = new byte[1024];
-		byte[] bufferEnvio = new byte[1024];
-		DatagramSocket dSocket = new DatagramSocket(123);
-		
+		// Socket para recibir y enviar datos datos
+		DatagramSocket socketLocal = new DatagramSocket(0123);
 		System.out.println("Esperando a recibir datagramas de un cliente...");
 		
-		// Recogemos el mesaje del cliente
-		DatagramPacket recibo = new DatagramPacket(buffer, buffer.length);
-		dSocket.receive(recibo);
+		while(true) {
+			
+			// Array de bytes para recibir y enviar datos
+			byte[] bufferRecibo = new byte[2048], bufferEnvio = new byte[2048];
+			
+			// Creamos las variables que utilizaremos para recibir y enviar cadenas
+			String mensajeEnviado, mensajeRecibido;
+			
+			/* =========================================== */
+			/* ========== RECEPCION DE PAQUETES ========== */
+			/* =========================================== */
+			
+			// Recogemos el mesaje del cliente
+			DatagramPacket recibo = new DatagramPacket(bufferRecibo, bufferRecibo.length);
+			socketLocal.receive(recibo);
+			
+			// Recogemos el mensaje del paquete que hemos recibido
+			mensajeRecibido = new String(recibo.getData());
+			System.out.println("Mensaje del cliente: " + mensajeRecibido);
+			System.out.println("IP del cliente: " + recibo.getAddress());
+			System.out.println("Puerto del cliente: " + recibo.getPort());
+			
+			/* ======================================= */
+			/* ========== ENVIO DE PAQUETES ========== */
+			/* ======================================= */
+			
+			// Construimos el mensaje que vamos a enviar
+			mensajeEnviado = mensajeRecibido+" - RECIBIDO POR EL SERVIDOR";
+			bufferEnvio = mensajeEnviado.getBytes();
+			
+			// Construimos el Datagrama para enviar y lo enviamos
+			DatagramPacket envio = new DatagramPacket(bufferEnvio, bufferEnvio.length, recibo.getAddress(), recibo.getPort());
+			socketLocal.send(envio);
+			
+			/* ================================================= */
+			/* ========== RESOLUCION DEL ENVIO/RECIBO ========== */
+			/* ================================================= */
+			
+			if (mensajeRecibido.trim().compareTo("*") == 0) break;
+			
+			mensajeEnviado = "";
+			mensajeRecibido = "";
+		}
 		
-		// Transformamos el mensaje en un array de bytes
-		String mensaje = recibo.getData() + " RECIBIDO POR EL SERVIDOR";
-		bufferEnvio = mensaje.getBytes();
-		
-		// Devolvemos el mensaje al cliente
-		DatagramPacket envio = new DatagramPacket(bufferEnvio, bufferEnvio.length);		
-		dSocket.send(envio);
-		
-		dSocket.close();
+		System.out.println("Conexión cerrada...");
+		socketLocal.close();
 	}
 }
