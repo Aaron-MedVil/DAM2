ff9db5cdb84476b41c5f7d5f6c2070c9a491269c
xaxe96 <a.medina.v.96@hotmail.com>
2021-02-19 12:23:15 +0100

    Update Cliente.java

--- PSP/procesos/src/Red/Ejercicio_03/Cliente.java
+++ PSP/procesos/src/Red/Ejercicio_03/Cliente.java
@@ -10,25 +10,64 @@ public class Cliente {
 
 	public static void main(String[] args) throws IOException {
 
+		// Creamos las variables que utilizaremos para recibir y enviar cadenas
+		String mensajeEnviado, mensajeRecibido;
+		
+		// Scanner para recoger datos introducidos por el teclado
 		Scanner teclado = new Scanner(System.in);
-		byte[] mensaje = new byte[1024];
 
-		// IP al que enviamos el mensaje
-		InetAddress destino = InetAddress.getLocalHost();
+		// IP del servidor
+		InetAddress destino = InetAddress.getByName("localhost");
+		int puertoEnvio = 0123;
 		
-		// Recogemos el mensaje que vamos a enviar
-		System.out.println("Inserte un mensaje");
-		mensaje = teclado.nextLine().getBytes();
+		// Socket para recibir y enviar datos datos
+		DatagramSocket socketLocal = new DatagramSocket();
 		
-		// Construimos el Datagrama para enviar
-		DatagramPacket envio = new DatagramPacket(mensaje, mensaje.length, destino, 123);
-		DatagramSocket dSocket = new DatagramSocket(67890);
+		while(true) {
+			
+			// Array de bytes para recibir y enviar datos
+			byte[] bufferRecibo = new byte[2048], bufferEnvio = new byte[2048];
+			
+			/* ======================================= */
+			/* ========== ENVIO DE PAQUETES ========== */
+			/* ======================================= */
+			
+			// Recogemos el mensaje que vamos a enviar
+			System.out.println("Inserte un mensaje");
+			mensajeEnviado = teclado.nextLine();
+			bufferEnvio = mensajeEnviado.getBytes();
+			
+			// Construimos el Datagrama para enviar y lo enviamos
+			DatagramPacket envio = new DatagramPacket(bufferEnvio, bufferEnvio.length, destino, puertoEnvio);
+			socketLocal.send(envio);
+			
+			/* =========================================== */
+			/* ========== RECEPCION DE PAQUETES ========== */
+			/* =========================================== */
+			
+			// Recogemos el mesaje del cliente
+			DatagramPacket recibo = new DatagramPacket(bufferRecibo, bufferRecibo.length);
+			socketLocal.receive(recibo);
+						
+			// Recogemos el mensaje del paquete que hemos recibido
+			mensajeRecibido = new String(recibo.getData());
+			System.out.println("Mensaje del servidor: " + mensajeRecibido);
+			
+			/* ================================================= */
+			/* ========== RESOLUCION DEL ENVIO/RECIBO ========== */
+			/* ================================================= */
+			
+			if (mensajeEnviado.trim().compareTo("*") == 0) break;
+			
+			mensajeEnviado = "";
+			mensajeRecibido = "";
+		}
 		
-		// Envio el datagrama
-		dSocket.send(envio);
+		System.out.println("Conexión cerrada...");
 				
-		// Cerramos el socket
-		dSocket.close();
+		// Cerramos los flujos
+		socketLocal.close();
+		teclado.close();
 	}
 
 }
